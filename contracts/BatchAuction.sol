f42d4ff1dd70251b29edaf0f12eab9eae49837dc
Ben Smith <bh2smith@gmail.com>
2019-01-08 18:25:39 +0100

    Update Contract with addToken functionality ownership and Unit Tests

--- contracts/BatchAuction.sol
+++ contracts/BatchAuction.sol
@@ -1,12 +1,22 @@
 pragma solidity ^0.5.0;
 
+// import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract BatchAuction {
+
+contract BatchAuction is Ownable {
 
     uint16 public constant MAX_ACCOUNT_ID = 100;
+    uint8 public constant MAX_TOKENS = 30;
 
     mapping (address => uint16) public publicKeyToAccountMap;
     mapping (uint16 => address) public accountToPublicKeyMap;
+
+    uint8 public numTokens;
+    mapping (address => uint8) public tokenAddresToIdMap;
+    mapping (uint8 => address) public tokenIdToAddressMap;
+
+    event RegisteredToken(address tokenAddress, uint8 tokenId);
 
     function openAccount(uint16 accountId) public {
         require(accountId != 0, "Account index must be positive!");
@@ -19,4 +29,16 @@ contract BatchAuction {
         publicKeyToAccountMap[msg.sender] = accountId;
         accountToPublicKeyMap[accountId] = msg.sender;
     }
+
+    function addToken(address _tokenAddress) public onlyOwner() {
+        require(tokenAddresToIdMap[_tokenAddress] == 0, "Token already registered!");
+        require(numTokens + 1 <= MAX_TOKENS, "Token id exceeds max tokens");
+
+        tokenAddresToIdMap[_tokenAddress] = numTokens + 1;
+        tokenIdToAddressMap[numTokens + 1] = _tokenAddress;
+
+        numTokens++;
+        emit RegisteredToken(_tokenAddress, numTokens);
+    }
+
 }
