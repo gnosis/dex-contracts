8293c53dc42efedc1c7fb86557be08a830ba242f
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-01-14 13:46:38 +0100

    Apply deposit (#4)
    * include mintable token in DevDependencies so that we can test with Mintable Tokens
    
    * [Temp] Elementary deposit function (will need to used depositHash array)
    
    * extra approval unnecessary
    
    * update unit tests
    
    * update travis scripts (more straightforward)
    
    * use array of deposit hashes (for roll backs) and applied bool to determine if its already been applied
    
    * test depositSlot incrementer
    
    * update gitignore
    
    * include first few usefull utility functions and
    
    * use new utility functions
    
    * [WIP] first pass through applyDeposits
    
    * apply suggestion - no need to push empty deposit hashes
    
    * no need for Pedersen Struct
    
    * new utility functions for environment setup
    
    - approveContract
    - openAccounts
    - registerTokens
    
    * remove redundant requirement
    
    * depositIndex is now a public view (as it is computed directly from EVM block number)
    
    * Complete unit tests for applyDeposit (with dummy values)
    
    Signed-off-by: Ben Smith <bh2smith@gmail.com>
    
    * clean up revert strings
    
    * remove superfluous information in last two unit tests
    
    * include suggestions from reviewers
    
    * more general unit tests with non-trivial hash values extracted from the contract.

--- contracts/BatchAuction.sol
+++ contracts/BatchAuction.sol
@@ -9,6 +9,14 @@ contract BatchAuction is Ownable {
     uint16 public constant MAX_ACCOUNT_ID = 100;
     uint8 public constant MAX_TOKENS = 30;
 
+    bytes32[] public stateRoots;  // Pedersen Hash
+
+    enum TransitionType {
+        Deposit,
+        Withdrawal,
+        Auction
+    }
+
     mapping (address => uint16) public publicKeyToAccountMap;
     mapping (uint16 => address) public accountToPublicKeyMap;
 
@@ -21,15 +29,19 @@ contract BatchAuction is Ownable {
         bool applied;
     }
     
-    uint public depositIndex;
     mapping (uint => DepositState) public depositHashes;
 
     event Deposit(uint16 accountId, uint8 tokenIndex, uint amount, uint slot);
-    event IncrementDepositIndex(uint index);
+    event StateTransistion(TransitionType transitionType, bytes32 from, bytes32 to);
 
     modifier onlyRegistered() {
         require(publicKeyToAccountMap[msg.sender] != 0, "Must have registered account");
         _;
+    }
+
+    constructor () public {
+        // The initial state should be Pederson hash of an empty balance tree
+        stateRoots.push(0);
     }
 
     function openAccount(uint16 accountId) public {
@@ -59,24 +71,45 @@ contract BatchAuction is Ownable {
 
         address tokenAddress = tokenIdToAddressMap[tokenIndex];
         require(tokenAddress != address(0), "Requested token is not registered");
-
         require(
             ERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), 
             "Unsuccessful transfer"
         );
-        
-        if (depositIndex != block.number / 20) {
-            depositIndex = block.number / 20;
-            emit IncrementDepositIndex(depositIndex);
-        }
 
+        uint depositSlot = this.depositIndex();
         uint16 accountId = publicKeyToAccountMap[msg.sender];
         bytes32 nextDepositHash = sha256(
-            abi.encodePacked(depositHashes[depositIndex].shaHash, accountId, tokenIndex, amount)
+            abi.encodePacked(depositHashes[depositSlot].shaHash, accountId, tokenIndex, amount)
         );
 
-        depositHashes[depositIndex] = DepositState({shaHash: nextDepositHash, applied: false});
+        depositHashes[depositSlot] = DepositState({shaHash: nextDepositHash, applied: false});
+        emit Deposit(accountId, tokenIndex, amount, depositSlot);
+    }
 
-        emit Deposit(accountId, tokenIndex, amount, depositIndex);
+    function depositIndex() public view returns (uint) {
+        return block.number / 20;
+    }
+
+    function stateIndex() public view returns (uint) {
+        return stateRoots.length - 1;
+    }
+
+    function applyDeposits(
+        uint slot,
+        bytes32 _currDepositHash,
+        bytes32 _currStateRoot,
+        bytes32 _newStateRoot
+    )
+        public onlyOwner()
+    {   
+        require(slot < this.depositIndex(), "Deposit slot must exist and be inactive");
+        require(depositHashes[slot].applied == false, "Deposits already processed");
+        require(depositHashes[slot].shaHash != bytes32(0), "Deposit slot is empty");
+        require(depositHashes[slot].shaHash == _currDepositHash, "Incorrect Deposit Hash");
+        require(stateRoots[this.stateIndex()] == _currStateRoot, "Incorrect State Root");
+
+        stateRoots.push(_newStateRoot);        
+        depositHashes[slot].applied = true;
+        emit StateTransistion(TransitionType.Deposit, _currStateRoot, _newStateRoot);
     }
 }
--- test/utilities.js
+++ test/utilities.js
@@ -1,3 +1,5 @@
+const MintableERC20 = artifacts.require("./ERC20Mintable.sol")
+
 /*
  How to avoid using try/catch blocks with promises' that could fail using async/await
  - https://blog.grossman.io/how-to-write-async-await-without-try-catch-blocks-in-javascript/
@@ -20,14 +22,14 @@ const assertRejects = async (q, msg) => {
 /**
  * funds accounts with specified value for Mintable Token
  * The object consists of:
- * 1.) owner of token (i.e. minter)
+ * 1.) minter/owner of token (i.e. minter)
  * 2.) list of accounts
  * 3.) ERC20Mintable token
  * 4.) amount to be funded
  */
-const fundAccounts = async function(owner, accounts, token, amount) {
+const fundAccounts = async function(minter, accounts, token, amount) {
   for (let i = 0; i < accounts.length; i++) {
-    await token.mint(accounts[i], amount, { from: owner})
+    await token.mint(accounts[i], amount, { from: minter})
   }
 }
 
@@ -45,6 +47,55 @@ const approveContract = async function(contract, accounts, token, value) {
   }
 }
 
+/**
+ * opens accounts at their index + 1 on contract
+ * The object consists of:
+ * 1.) contract to register account
+ * 2.) list of accounts
+ */
+const openAccounts = async function(contract, accounts) {
+  for (let i = 0; i < accounts.length; i++) {
+    await contract.openAccount(i + 1, { from: accounts[i] })
+  }
+}
+
+/**
+ * depoloys and registers tokens on contract 
+ * The object consists of:
+ * 1.) contract to register account
+ * 2.) owner of contract
+ * 3.) number of tokens to be registered 
+ */
+const registerTokens = async function(contract, token_owner, numTokens) {
+  const res = []
+  const owner = await contract.owner()
+  for (let i = 0; i < numTokens; i++) {
+    const token = await MintableERC20.new({ from: token_owner })
+    res.push(token)
+    await contract.addToken(token.address, { from: owner })
+  }
+  return res
+}
+
+/**
+ * depoloys tokens, funds opens accounts, approves contract for transfer and opens accounts 
+ * The object consists of:
+ * 1.) BatchAuction Contract
+ * 2.) desired token owner (ideally not contract owner)
+ * 3.) accounts to be funded and registered
+ * 4.) number of tokens to be registered
+ * @returns {Array} tokens
+ */
+const setupEnvironment = async function(contract, token_owner, accounts, numTokens) {
+  const tokens = await registerTokens(contract, token_owner, numTokens)
+  for (let i = 0; i < tokens.length; i++) {openAccounts
+    await fundAccounts(token_owner, accounts, tokens[i], 100)
+    await approveContract(contract, accounts, tokens[i], 100)
+  }
+  await openAccounts(contract, accounts)
+  return tokens
+}
+
 // Wait for n blocks to pass
 const waitForNBlocks = async function(numBlocks, authority) {
   for (let i = 0; i < numBlocks; i++) {
@@ -57,4 +108,7 @@ module.exports = {
   waitForNBlocks,
   fundAccounts,
   approveContract,
+  openAccounts,
+  registerTokens,
+  setupEnvironment
 }
