077dfba485d6d274cc7afca6d483f8fe18eb9f5b
Ben Smith <bh2smith@gmail.com>
2019-01-21 15:17:57 +0100

    lint warnings

--- scripts/setup_initial_deposit.js
+++ scripts/setup_initial_deposit.js
@@ -1,16 +1,15 @@
-var SnappBase = artifacts.require("SnappBase");
-var ERC20Mintable = artifacts.require("ERC20Mintable");
+const SnappBase = artifacts.require("SnappBase")
+const ERC20Mintable = artifacts.require("ERC20Mintable")
 
 
-module.exports = async function(callback) {
-	const me = (await web3.eth.getAccounts())[0]
- 	const instance = await SnappBase.deployed()
- 	const r = await instance.openAccount(1)
- 	const token = await ERC20Mintable.new()
- 	await instance.addToken(token.address)
- 	await token.mint(me, 10)
- 	await token.approve(instance.address, 10)
- 	await instance.deposit(1, 1)
-
- 	console.log("new depositHash is:", await instance.depositHashes(0))
+module.exports = async function() {
+  const me = (await web3.eth.getAccounts())[0]
+  const instance = await SnappBase.deployed()
+  await instance.openAccount(1)
+  const token = await ERC20Mintable.new()
+  await instance.addToken(token.address)
+  await token.mint(me, 10)
+  await token.approve(instance.address, 10)
+  await instance.deposit(1, 1)
+  console.log("new depositHash is:", await instance.depositHashes(0))
 }
