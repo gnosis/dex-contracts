157f6934fc0a2394a0867edbd29937994f8dd89a
Ben Smith <bh2smith@gmail.com>
2019-01-23 15:08:47 +0100

    include apply deposits in package json and revert when deposit slot has already been applied with warning

--- scripts/apply_deposits.js
+++ scripts/apply_deposits.js
@@ -10,6 +10,10 @@ module.exports = async (callback) => {
     const state_index = (await instance.stateIndex.call()).toNumber()
     const curr_state = await instance.stateRoots.call(state_index)
 
+    if ((await instance.depositHashes.call(slot)).applied == true) {
+        console.log("Warning: Requested deposit slot has already been applied")
+        callback()
+    }
     const tx = await instance.applyDeposits(slot, curr_state, new_state)
     callback()
 }
