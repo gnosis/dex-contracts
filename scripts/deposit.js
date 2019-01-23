550fdac4e5e9521d5057d3585b6e81288c9a4f5f
Ben Smith <bh2smith@gmail.com>
2019-01-23 12:25:41 +0100

    use transaction event log to recover relevant infomation for logging.

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -14,10 +14,10 @@ module.exports = async (callback) => {
         console.log("No account registerd at index %s", accountId)
         callback()
     }
-    const slot = (await instance.depositSlot.call()).toNumber()
     const tx = await instance.deposit(tokenId, amount, {from: depositor})
+    const slot = tx.logs[0].args.slot.toNumber()
+    const slot_index =  tx.logs[0].args.slotIndex.toNumber()
 
-    const slot_index = (await instance.slotIndex.call()).toNumber() - 1
     const deposit_hash = (await instance.depositHashes(slot)).shaHash
     console.log("Deposit successful: Slot %s - Index %s - Hash %s", slot, slot_index, deposit_hash)
     callback()
