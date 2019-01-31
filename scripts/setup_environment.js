a38646e73e46e55db28566ac6363b54f9c9b2215
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-02-01 05:06:38 +1100

    suggestion
    Co-Authored-By: fleupold <felixleupold90@gmail.com>

--- scripts/setup_environment.js
+++ scripts/setup_environment.js
@@ -7,7 +7,6 @@ module.exports = async function(callback) {
     const instance = await SnappBase.deployed()
     const accounts = await web3.eth.getAccounts()
     const token_owner = accounts[1]
-  
     await setupEnvironment(ERC20Mintable, instance, token_owner, accounts, 10)
     console.log("Environment setup complete")
     callback()
