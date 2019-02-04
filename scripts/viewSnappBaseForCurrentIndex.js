521d410b46eb41757ea230ebd2140bd0f592d44a
josojo <josojo@hotmail.de>
2019-02-04 09:42:03 +0100

    replaced view stateroots more generic view function

--- scripts/viewSnappBaseForCurrentIndex.js
+++ scripts/viewSnappBaseForCurrentIndex.js
@@ -1,26 +1,21 @@
 const SnappBase = artifacts.require("SnappBase")
 
+// This script allows to view data from the SnappBase contract
+// example for viewing the current stateRoots: truffle exec scripts/viewSnappBaseForCurrentIndex.js 'stateRoots'
+
 module.exports = async (callback) => {
   try {
     const arguments = await process.argv.slice(4)
     if (arguments.length != 1) {
       callback("Error: This script requires arguments - <targetStateHash>")
     }
-    const [targetStateHash] = arguments
+    const [functionName] = arguments
     
     const instance = await SnappBase.deployed()
     const state_index = (await instance.stateIndex.call()).toNumber()
-    const curr_state = await instance.stateRoots.call(state_index)
-    try {
-      if (curr_state != targetStateHash) {
-        throw ("Error: stateHash not correct")
-      } 
-    } 
-    catch(error) {
-        throw new Error(error)
-    }
+    const info = await instance[functionName].call(state_index)
     
-    callback()
+    callback(info)
   } catch (error) {
     callback(error)
   }
