775809994576cba4822774261daaef5f0bd09cdd
josojo <josojo@hotmail.de>
2019-02-18 13:58:24 +0100

    indentation

--- scripts/script_utilities.js
+++ scripts/script_utilities.js
@@ -1,9 +1,9 @@
 
 const getArgumentsHelper = function() {
-    const arguments = process.argv.slice(4)
-    const index = arguments.indexOf("--network")
-    if (index > -1) {
-      arguments.splice(index, 2)
+  const arguments = process.argv.slice(4)
+  const index = arguments.indexOf("--network")
+  if (index > -1) {
+    arguments.splice(index, 2)
   }
   return arguments
 }
