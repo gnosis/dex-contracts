32ea8e3bee9a1251d32587808839755a18dcc520
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-02-19 13:54:59 +0100

    Request Withdraw Script (#41)
    * request withdraw script
    
    * add npm command request-withdraw
    
    * reverse wording
    
    * use getArgumentsHelper

---
+++ scripts/request_withdraw.js
@@ -0,0 +1,34 @@
+const SnappBase = artifacts.require("SnappBase")
+const zero_address = 0x0
+const getArgumentsHelper = require("./script_utilities.js")
+
+module.exports = async (callback) => {
+  try {
+    const arguments = getArgumentsHelper()
+    if (arguments.length != 3) {
+      callback("Error: This script requires arguments - <accountId> <tokenId> <withdrawAmount>")
+    }
+    const [accountId, tokenId, amount] = arguments
+    
+    const instance = await SnappBase.deployed()
+    const depositor = await instance.accountToPublicKeyMap.call(accountId)
+    if (depositor == zero_address) {
+      callback(`Error: No account registerd at index ${accountId}`)
+    }
+  
+    const token_address = await instance.tokenIdToAddressMap.call(tokenId)
+    if (token_address == zero_address) {
+      callback(`Error: No token registered at index ${tokenId}`)
+    }
+  
+    const tx = await instance.requestWithdrawal(tokenId, amount, {from: depositor})
+    const slot = tx.logs[0].args.slot.toNumber()
+    const slot_index = tx.logs[0].args.slotIndex.toNumber()
+  
+    const withdraw_hash = (await instance.pendingWithdraws(slot)).shaHash
+    console.log("Withdraw Request successful: Slot %s - Index %s - Hash %s", slot, slot_index, withdraw_hash)
+    callback()
+  } catch(error) {
+    callback(error)
+  }
+}
