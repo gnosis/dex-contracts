f8b39f503988c1299a104ab890ebad8ebcaffa75
Ben Smith <bh2smith@gmail.com>
2019-02-22 16:02:10 +0100

    full suite of test for claim withdrawal

--- scripts/test_claim_withdraw.js
+++ scripts/test_claim_withdraw.js
@@ -3,8 +3,13 @@ const processDeposits = require("./apply_deposits")
 const mineBlocks = require("./mine_blocks")
 const requestWithdraw = require("./request_withdraw")
 const processWithdrawals = require("./apply_withdrawals")
-// const claimWithdraw = require("./claim_withdraw")
+const claimWithdraw = require("./claim_withdraw")
 
+const { encodePacked_16_8_128 }  = require("../test/snapp_utils.js")
+const {
+  generateMerkleTree,
+  toHex
+} = require("../test/utilities.js")
 
 module.exports = async function(callback) {
   try {
@@ -17,6 +22,17 @@ module.exports = async function(callback) {
     await requestWithdraw(1, 2, 2)
     await mineBlocks(20)
     await processWithdrawals(0, 0x0, 0x0)
+    // Generate correct merkle root
+    const withdraw_hashes = [encodePacked_16_8_128(1, 1, 1), encodePacked_16_8_128(0, 0, 0)]
+    const tree = generateMerkleTree(withdraw_hashes)
+    const root = toHex(tree.getRoot())
+    await processWithdrawals(1, root, 0x0)
+    await claimWithdraw(1, 1, 1)
+
+    // Shoudl fail
+    await claimWithdraw(1, 1, 1)
+    // Should also fail
+    await claimWithdraw(1, 1, 2)
   } catch (error) {
     callback(error)
   }
