d6a78e994f4c4974220dcf7643163cd62bb86c28
Ben Smith <bh2smith@gmail.com>
2019-03-27 14:48:19 +0100

    Unit Testing placeSellOrder

--- test/snapp_auction.js
+++ test/snapp_auction.js
@@ -1,18 +1,63 @@
 const SnappAuction = artifacts.require("SnappAuction")
 const MintableERC20 = artifacts.require("./ERC20Mintable.sol")
 
-// const truffleAssert = require("truffle-assertions")
+const truffleAssert = require("truffle-assertions")
 
 const { setupEnvironment } = require("./utilities.js")
 
 contract("SnappBase", async (accounts) => {
-  const [token_owner, user_1] = accounts
+  const [token_owner, user_1, user_2] = accounts
 
   describe("placeSellOrder()", () => {
-    it.only("Generic sell order", async () => {
+    it("Reject: unregisterd account", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 1, 1, 1, { from: user_2 }),
+        "Must have registered account"
+      )
+    })
+
+    it("Reject: buyToken = sellToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 1, 1, 1, { from: user_1 }),
+        "Buy and Sell tokens must differ!"
+      )
+    })
+
+    it("Reject: unregistered buyToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(3, 1, 1, 1, { from: user_1 }),
+        "Buy token is not registered"
+      )
+    })
+
+    it("Reject: unregistered sellToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 3, 1, 1, { from: user_1 }),
+        "Sell token is not registered"
+      )
+    })
+
+    it("Generic sell order", async () => {
       const instance = await SnappAuction.new()
       await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
       await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      const auctionIndex = (await instance.auctionIndex.call()).toNumber()
+      const currentAuction = await instance.auctions(auctionIndex)
+
+      assert.equal(currentAuction.size, 1)
+      assert.notEqual(currentAuction.shaHash, 0)
     })
   })
 })
