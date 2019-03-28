901097f96fb0d98f1ea4724d37d16ee36d0eddba
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-03-28 15:10:34 +0100

    #54 Order Collection (#58)
    * First pass through SnappAuction sellOrder collection.
    
    * Unit Testing placeSellOrder
    
    * 100% Branch coverage on placeOrder
    
    * - PendingFlux to PendingBatch
    - Buy and Sell amounts constrained to have at most 100 bits.
    -
    
    * reject all incomming orders if previous two auctions haven't been applied.
    
    * Move order collection half into new batch branch. Unit testing all possible cases. Can't test passing this case, becuase we don't have state transition yet.
    
    * fix unit test (extra await)
    
    * Remove buyToken != sellToken constraint (i.e. allow limit orders between same tokens).
    
    * Rename buy and sell variables in SellOrder collection
    
    * nested if and require into "one-liner"
    
    * better line breaking

---
+++ contracts/SnappAuction.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.5.0;
+
+import "./SnappBase.sol";
+
+
+contract SnappAuction is SnappBase {
+  
+    uint16 public constant AUCTION_BATCH_SIZE = 1000;
+
+    uint public auctionIndex;
+    mapping (uint => PendingBatch) public auctions;
+
+    event SellOrder(
+        uint auctionId, 
+        uint16 slotIndex,
+        uint16 accountId, 
+        uint8 buyToken, 
+        uint8 sellToken, 
+        uint128 buyAmount,
+        uint128 sellAmount
+    );
+
+    constructor () public {
+        auctions[auctionIndex].creationBlock = block.number;
+    }
+
+    function placeSellOrder(
+        uint8 buyToken,
+        uint8 sellToken,
+        uint128 buyAmount,
+        uint128 sellAmount
+    ) public onlyRegistered() {
+        // Must have 0 < tokenId < MAX_TOKENS anyway, so may as well ensure registered.
+        require(tokenIdToAddressMap[buyToken] != address(0), "Buy token is not registered");
+        require(tokenIdToAddressMap[sellToken] != address(0), "Sell token is not registered");
+
+        // Could also enforce that buyToken != sellToken, but not technically illegal.
+
+        if (
+            auctions[auctionIndex].size == AUCTION_BATCH_SIZE || 
+            block.number > auctions[auctionIndex].creationBlock + 20
+        ) {
+            require(
+                auctionIndex < 2 || auctions[auctionIndex - 2].appliedAccountStateIndex != 0,
+                "Too many pending auctions"
+            );
+            auctionIndex++;
+            auctions[auctionIndex] = PendingBatch({
+                size: 0,
+                shaHash: bytes32(0),
+                creationBlock: block.number,
+                appliedAccountStateIndex: 0
+            });
+        }
+
+        // Update Auction Hash based on request
+        uint16 accountId = publicKeyToAccountMap[msg.sender];
+        bytes32 nextAuctionHash = sha256(
+            abi.encodePacked(
+                auctions[auctionIndex].shaHash,
+                encodeOrder(accountId, buyToken, sellToken, buyAmount, sellAmount)
+            )
+        );
+        auctions[auctionIndex].shaHash = nextAuctionHash;
+
+        emit SellOrder(auctionIndex, auctions[auctionIndex].size, accountId, buyToken, sellToken, buyAmount, sellAmount);
+        // Only increment size after event (so it is emitted as an index)
+        auctions[auctionIndex].size++;
+    }
+
+    function encodeOrder(
+        uint16 accountId, 
+        uint8 buyToken, 
+        uint8 sellToken, 
+        uint128 buyAmount, 
+        uint128 sellAmount
+    ) internal pure returns (bytes32) {
+        // Restrict buy and sell amount to occupy at most 100 bits.
+        require(buyAmount < 0x10000000000000000000000000, "Buy amount too large!");
+        require(sellAmount < 0x10000000000000000000000000, "Sell amount too large!");
+
+        // solhint-disable-next-line max-line-length
+        return bytes32(uint(accountId) + (uint(buyToken) << 16) + (uint(sellToken) << 24) + (uint(sellAmount) << 32) + (uint(buyAmount) << 132));
+    }
+}
---
+++ test/snapp_auction.js
@@ -0,0 +1,105 @@
+const SnappAuction = artifacts.require("SnappAuction")
+const MintableERC20 = artifacts.require("./ERC20Mintable.sol")
+
+const truffleAssert = require("truffle-assertions")
+
+const {
+  waitForNBlocks,
+  setupEnvironment } = require("./utilities.js")
+
+contract("SnappAuction", async (accounts) => {
+  const [owner, token_owner, user_1, user_2] = accounts
+
+  describe("placeSellOrder()", () => {
+    it("Reject: unregisterd account", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 2, 1, 1, { from: user_2 }),
+        "Must have registered account"
+      )
+    })
+
+    it("Reject: unregistered buyToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(3, 1, 1, 1, { from: user_1 }),
+        "Buy token is not registered"
+      )
+    })
+
+    it("Reject: unregistered sellToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 3, 1, 1, { from: user_1 }),
+        "Sell token is not registered"
+      )
+    })
+
+    it("Reject: Buy Amount >= 2^100", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 2, "0x10000000000000000000000000", 1, { from: user_1 }),
+        "Buy amount too large!"
+      )
+    })
+
+    it("Reject: Sell Amount >= 2^100", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 2, 1, "0x10000000000000000000000000", { from: user_1 }),
+        "Sell amount too large!"
+      )
+    })
+
+    it("Reject: Third batch with two unapplied", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      await waitForNBlocks(21, owner)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      await waitForNBlocks(21, owner)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      await waitForNBlocks(21, owner)
+      await truffleAssert.reverts(
+        instance.placeSellOrder(1, 2, 1, 1, { from: user_1 }),
+        "Too many pending auctions"
+      )
+    })
+
+    it("Generic sell order", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      const auctionIndex = (await instance.auctionIndex.call()).toNumber()
+      const currentAuction = await instance.auctions(auctionIndex)
+
+      assert.equal(currentAuction.size, 1)
+      assert.notEqual(currentAuction.shaHash, 0)
+    })
+
+    it("Generic sell orders over two batches", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      await waitForNBlocks(21, owner)
+      await instance.placeSellOrder(1, 2, 1, 1, { from: user_1 })
+
+      const auctionIndex = (await instance.auctionIndex.call()).toNumber()
+      assert.equal(auctionIndex, 1)
+    })
+  })
+})
