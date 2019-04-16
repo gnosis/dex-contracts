b59cc9c7d3beb5a1ec22f93a66e31be36de259a2
Ben Smith <bh2smith@gmail.com>
2019-04-16 10:20:45 +0200

    closes #69

--- scripts/setup_environment.js
+++ scripts/setup_environment.js
@@ -1,14 +1,17 @@
 const SnappAuction = artifacts.require("SnappAuction")
 const ERC20Mintable = artifacts.require("ERC20Mintable.sol")
-
+const getArgumentsHelper = require("./script_utilities.js")
 const { setupEnvironment } = require("../test/utilities.js")
 
 module.exports = async function(callback) {
   try {
+
+    const arguments = getArgumentsHelper()
+    const [numAccounts] = arguments || 3
     const instance = await SnappAuction.deployed()
     const accounts = await web3.eth.getAccounts()
     const token_owner = accounts[1]
-    await setupEnvironment(ERC20Mintable, instance, token_owner, accounts, 10)
+    await setupEnvironment(ERC20Mintable, instance, token_owner, accounts, numAccounts)
     console.log("Environment setup complete")
     callback()
   } catch (error) {
