c8f452de6a2a5c4a2858df1bcabfec0494923770
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-04-16 11:42:28 +0200

    closes #69 (#71)
    * closes #69
    
    * default 3 on both accounts and tokens

--- scripts/setup_environment.js
+++ scripts/setup_environment.js
@@ -1,14 +1,21 @@
 const SnappAuction = artifacts.require("SnappAuction")
 const ERC20Mintable = artifacts.require("ERC20Mintable.sol")
-
+const getArgumentsHelper = require("./script_utilities.js")
 const { setupEnvironment } = require("../test/utilities.js")
 
 module.exports = async function(callback) {
   try {
+    const arguments = getArgumentsHelper()
+    if (arguments.length > 2) {
+      callback("Error: This script accepts optional arguments - <numAccounts=3> <numTokens=3>")
+    }
+    const numAccounts = arguments[0] || 3
+    const numTokens = arguments[1] || 3
     const instance = await SnappAuction.deployed()
     const accounts = await web3.eth.getAccounts()
     const token_owner = accounts[1]
-    await setupEnvironment(ERC20Mintable, instance, token_owner, accounts, 10)
+    console.log(`Beginning environment setup with ${numAccounts} accounts and ${numTokens} tokens`)
+    await setupEnvironment(ERC20Mintable, instance, token_owner, accounts.slice(numAccounts), numTokens)
     console.log("Environment setup complete")
     callback()
   } catch (error) {
