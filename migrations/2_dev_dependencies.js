2f90481a2c3b9f5c6c91b6161e19188638b761a2
Ben Smith <bh2smith@gmail.com>
2019-04-24 09:24:22 +0200

    New Library BiMap - An interface for Bijective maps

--- migrations/2_dev_dependencies.js
+++ migrations/2_dev_dependencies.js
@@ -1,7 +1,14 @@
 /*eslint no-undef: "off"*/
 
 const Dependencies = artifacts.require("./DevDependencies.sol")
+const BiMap = artifacts.require("BiMap.sol")
+const BiMapWrapper = artifacts.require("BiMapWrapper.sol")
 
 module.exports = function(deployer) {
   deployer.deploy(Dependencies)
+
+  deployer.deploy(BiMap).then(() => {
+    deployer.deploy(BiMapWrapper)
+  })
+  deployer.link(BiMap, BiMapWrapper)
 }
--- migrations/3_batch_auction.js
+++ migrations/3_batch_auction.js
@@ -1,7 +1,18 @@
 /*eslint no-undef: "off"*/
 
+// const SnappAuction = artifacts.require("./SnappAuction.sol")
+
+// module.exports = function(deployer) {
+//   deployer.deploy(SnappAuction)
+// }
+
+
+const BiMap = artifacts.require("BiMap.sol")
 const SnappAuction = artifacts.require("./SnappAuction.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(SnappAuction)
+module.exports = function (deployer) {
+  deployer.deploy(BiMap).then(() => {
+    deployer.deploy(SnappAuction)
+  })
+  deployer.link(BiMap, SnappAuction)
 }
