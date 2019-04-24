522287f4cd6541e1fbf28b064972f128339111d1
Felix Leupold <felixleupold90@gmail.com>
2019-04-24 13:35:14 +0200

    adjust wait script

---
+++ scripts/wait_seconds.js
@@ -0,0 +1,17 @@
+const { waitForNSeconds } = require("../test/utilities.js")
+const getArgumentsHelper = require("./script_utilities.js")
+
+module.exports = async (callback) => {
+  try {
+    const arguments = getArgumentsHelper()
+    if (arguments.length != 1) {
+      callback("Error: This script requires arguments - <seconds>")
+    }
+    const [seconds] = arguments
+    await waitForNSeconds(seconds, web3)
+    console.log("waited", seconds, "seconds")
+    callback()
+  } catch(error) {
+    callback(error)
+  }
+}
