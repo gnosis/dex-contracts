8dd9f7e10bf3e2b269e33542a97b8d51f228dbff
Ben Smith <bh2smith@gmail.com>
2019-04-25 10:41:03 +0200

    this works!

--- migrations/3_batch_auction.js
+++ migrations/3_batch_auction.js
@@ -4,7 +4,8 @@ const BiMap = artifacts.require("IdToAddressBiMap.sol")
 const SnappAuction = artifacts.require("./SnappAuction.sol")
 
 module.exports = function (deployer) {
-  deployer.deploy(BiMap).then(() => {
-    deployer.link(BiMap, SnappAuction)
-  })
+  deployer.deploy(BiMap)
+  deployer.link(BiMap, SnappAuction)
+  deployer.deploy(SnappAuction)
+  
 }
