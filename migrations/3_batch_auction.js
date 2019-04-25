0bc66315568bb2eb46a7715191848adc60d959c2
Ben Smith <bh2smith@gmail.com>
2019-04-25 10:16:45 +0200

    closes #80

--- migrations/3_batch_auction.js
+++ migrations/3_batch_auction.js
@@ -5,8 +5,6 @@ const SnappAuction = artifacts.require("./SnappAuction.sol")
 
 module.exports = function (deployer) {
   deployer.deploy(BiMap).then(() => {
-    deployer.link(BiMap, SnappAuction).then(() => {
-      deployer.deploy(SnappAuction)
-    })
+    deployer.link(BiMap, SnappAuction)
   })
 }
