18b76664f0de6572a7998b1f06ea9537f6d2489b
josojo <josojo@hotmail.de>
2019-06-06 17:13:24 +0200

    Standing Orders Submission and Reserved Accounts Compatibility  (#97)
    * first draft of Standing Orders
    
    * linting and other small corrections
    
    * remove one more comment for linting
    
    * make sure that auctionIndex is correct during order placement
    
    * minor correction
    
    * ben suggestions
    
    * refactoring createNewAuctionBatch()
    
    * more mini-refactors according to Bens suggestions
    
    * adding new tests and make order submission cleaner
    
    * some refactorings based on suggestions
    
    * small refactorings
    
    * adjust tests minimally
    
    * Update contracts/SnappAuction.sol
    
    Co-Authored-By: Benjamin Smith <bh2smith@users.noreply.github.com>
    
    * Felix improvementsuggestions
    
    * fix broken test
    
    * fixing test for real
    
    * replaced nonce by currentCnt
    
    * feature: orders are only valid until validTill specified in order submission
    
    * renaming as suggested by Felix
    
    * reverting order validity limitations and put in a further TODO
    
    * Bens naming suggestion

--- contracts/SnappAuction.sol
+++ contracts/SnappAuction.sol
@@ -8,6 +8,19 @@ contract SnappAuction is SnappBase {
     uint16 public constant AUCTION_BATCH_SIZE = 1000;
     uint8 public constant AUCTION_RESERVED_ACCOUNTS = 50;
     uint8 public constant AUCTION_RESERVED_ACCOUNT_BATCH_SIZE = 10;
+    
+    struct StandingOrderBatch {
+        bytes32 orderHash;
+        uint validFromIndex; // validity is inclusive of the auction index
+        // validToIndex is indirectly given as validFromIndex of next Orderbatch
+    }
+
+    struct StandingOrderData {
+        mapping(uint => StandingOrderBatch) reservedAccountOrders;
+        uint currentBatchIndex;
+    }
+
+    mapping (uint16 => StandingOrderData) public standingOrders;
 
     uint public auctionIndex = MAX_UINT;
     mapping (uint => PendingBatch) public auctions;
@@ -20,6 +33,15 @@ contract SnappAuction is SnappBase {
         uint8 sellToken,
         uint128 buyAmount,
         uint128 sellAmount
+    );
+
+    event StandingSellOrderBatch(
+        uint currentBatchIndex, 
+        uint16 accountId, 
+        uint8[] buyToken, 
+        uint8[] sellToken, 
+        uint128[] buyAmount,
+        uint128[] sellAmount
     );
 
     event AuctionSettlement(
@@ -56,37 +78,90 @@ contract SnappAuction is SnappBase {
         return auctions[slot].appliedAccountStateIndex != 0;
     }
 
+    function getStandingOrderHash(uint16 userId, uint128 batchIndex) public view returns (bytes32) {
+        return standingOrders[userId].reservedAccountOrders[batchIndex].orderHash;
+    }
+    
+    function getStandingOrderValidFrom(uint16 userId, uint128 batchIndex) public view returns (uint) {
+        return standingOrders[userId].reservedAccountOrders[batchIndex].validFromIndex;
+    }
+
+    function getStandingOrderValidTo(uint16 userId, uint128 batchIndex) public view returns (uint) {
+        uint validTo = standingOrders[userId].reservedAccountOrders[batchIndex + 1].validFromIndex;
+        if (validTo == 0) {
+            return MAX_UINT;
+        } else {
+            return validTo - 1; 
+        }
+    }
+
+    function getStandingOrderCounter(uint16 userId) public view returns (uint) {
+        return standingOrders[userId].currentBatchIndex;
+    }
+
     /**
      * Auction Functionality
      */
+    function placeStandingSellOrder(
+        uint8[] memory buyTokens,
+        uint8[] memory sellTokens,
+        uint128[] memory buyAmounts,
+        uint128[] memory sellAmounts
+    ) public onlyRegistered() {
+        
+        // Update Auction Hash based on request
+        uint16 accountId = publicKeyToAccountMap(msg.sender);
+        require(accountId <= AUCTION_RESERVED_ACCOUNTS, "Accout is not a reserved account");
+
+        bytes32 orderHash;
+        uint numOrders = buyTokens.length;
+        require(numOrders <= AUCTION_RESERVED_ACCOUNT_BATCH_SIZE, "Too many orders for reserved batch");
+        
+        if (
+            auctionIndex == MAX_UINT ||
+            block.timestamp > (auctions[auctionIndex].creationTimestamp + 3 minutes)
+        ) {
+            createNewPendingBatch();
+        }
+
+        for (uint i = 0; i < numOrders; i++) {
+            orderHash = sha256(
+                abi.encodePacked(
+                    orderHash,
+                    encodeOrder(accountId, buyTokens[i], sellTokens[i], buyAmounts[i], sellAmounts[i])
+                )
+            );
+        }        
+        uint currentBatchIndex = standingOrders[accountId].currentBatchIndex;
+        StandingOrderBatch memory standingOrderBatch = standingOrders[accountId].reservedAccountOrders[currentBatchIndex];
+        if (auctionIndex > standingOrderBatch.validFromIndex) {
+            currentBatchIndex = currentBatchIndex + 1;
+            standingOrders[accountId].currentBatchIndex = currentBatchIndex;
+            standingOrderBatch = standingOrders[accountId].reservedAccountOrders[currentBatchIndex];
+            standingOrderBatch.validFromIndex = auctionIndex;
+            standingOrderBatch.orderHash = orderHash;
+        } else {
+            standingOrderBatch.orderHash = orderHash;
+        }
+        //TODO: The case auctionIndex < standingOrderBatch.validFromIndex can happen once roll-backs are implemented
+        //Then we have to revert the orderplacement
+        standingOrders[accountId].reservedAccountOrders[currentBatchIndex] = standingOrderBatch;
+        emit StandingSellOrderBatch(currentBatchIndex, accountId, buyTokens, sellTokens, buyAmounts, sellAmounts);
+    }
+
     function placeSellOrder(
         uint8 buyToken,
         uint8 sellToken,
         uint128 buyAmount,
         uint128 sellAmount
     ) public onlyRegistered() {
-        // Must have 0 < tokenId < MAX_TOKENS anyway, so may as well ensure registered.
-        require(buyToken < numTokens, "Buy token is not registered");
-        require(sellToken < numTokens, "Sell token is not registered");
-
-        // Could also enforce that buyToken != sellToken, but not technically illegal.
 
         if (
             auctionIndex == MAX_UINT ||
             auctions[auctionIndex].size == maxUnreservedOrderCount() ||
             block.timestamp > (auctions[auctionIndex].creationTimestamp + 3 minutes)
         ) {
-            require(
-                auctionIndex == MAX_UINT || auctionIndex < 2 || auctions[auctionIndex - 2].appliedAccountStateIndex != 0,
-                "Too many pending auctions"
-            );
-            auctionIndex++;
-            auctions[auctionIndex] = PendingBatch({
-                size: 0,
-                shaHash: bytes32(0),
-                creationTimestamp: block.timestamp,
-                appliedAccountStateIndex: 0
-            });
+            createNewPendingBatch();
         }
 
         // Update Auction Hash based on request
@@ -139,12 +214,18 @@ contract SnappAuction is SnappBase {
         uint8 sellToken,
         uint128 buyAmount,
         uint128 sellAmount
-    )
-        internal pure returns (bytes32)
+    ) 
+        internal view returns (bytes32) 
     {
         // Restrict buy and sell amount to occupy at most 96 bits.
         require(buyAmount < 0x1000000000000000000000000, "Buy amount too large!");
         require(sellAmount < 0x1000000000000000000000000, "Sell amount too large!");
+
+        // Must have 0 < tokenId < MAX_TOKENS anyway, so may as well ensure registered.
+        require(buyToken < numTokens, "Buy token is not registered");
+        require(sellToken < numTokens, "Sell token is not registered");
+
+        // Could also enforce that buyToken != sellToken, but not technically illegal.
 
         // solhint-disable-next-line max-line-length
         return bytes32(uint(accountId) + (uint(buyToken) << 16) + (uint(sellToken) << 24) + (uint(sellAmount) << 32) + (uint(buyAmount) << 128));
@@ -152,5 +233,19 @@ contract SnappAuction is SnappBase {
 
     function maxUnreservedOrderCount() internal pure returns (uint16) {
         return AUCTION_BATCH_SIZE - (AUCTION_RESERVED_ACCOUNTS * AUCTION_RESERVED_ACCOUNT_BATCH_SIZE);
+    }
+
+    function createNewPendingBatch() internal {
+        require(
+                auctionIndex == MAX_UINT || auctionIndex < 2 || auctions[auctionIndex - 2].appliedAccountStateIndex != 0,
+                "Too many pending auctions"
+            );
+        auctionIndex++;
+        auctions[auctionIndex] = PendingBatch({
+            size: 0,
+            shaHash: bytes32(0),
+            creationTimestamp: block.timestamp,
+            appliedAccountStateIndex: 0
+        });
     }
 }
--- test/snapp_auction.js
+++ test/snapp_auction.js
@@ -128,6 +128,120 @@ contract("SnappAuction", async (accounts) => {
     })
   })
 
+  describe("placeStandingSellOrder()", () => {
+    it("Reject: unregisterd account", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([0], [1], [1], [1], { from: user_2 }),
+        "Must have registered account"
+      )
+    })
+
+    it("Reject: unregistered buyToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([3], [1], [1], [1], { from: user_1 }),
+        "Buy token is not registered"
+      )
+    })
+
+    it("Reject: unregistered sellToken", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([1], [3], [1], [1], { from: user_1 }),
+        "Sell token is not registered"
+      )
+    })
+
+    it("Reject: Buy Amount >= 2^100", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([0], [1], ["0x10000000000000000000000000"], [1], { from: user_1 }),
+        "Buy amount too large!"
+      )
+    })
+
+    it("Reject: Sell Amount >= 2^100", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([0], [1], [1], ["0x10000000000000000000000000"], { from: user_1 }),
+        "Sell amount too large!"
+      )
+    })
+
+    it("Reject: More than AUCTION_RESERVED_ACCOUNT_BATCH_SIZE=10 orders", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await truffleAssert.reverts(
+        instance.placeStandingSellOrder([0,0,0,1,1,1,1,1,1,1,2,0,1], [1], [1], ["0x10000000000000000000000000"], { from: user_1 }),
+        "Too many orders for reserved batch"
+      )
+    })
+
+    it("Generic standing sell order as replacement of current batch (1 TX)", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await instance.placeStandingSellOrder([0,0], [0,1], [3,1], [3,1], { from: user_1 })
+
+      const userId = await instance.publicKeyToAccountMap.call(user_1)
+      const pointer = await instance.getStandingOrderCounter.call(userId)
+      assert.equal(pointer, 0)
+      const validFromIndex = await instance.getStandingOrderValidFrom(userId, pointer)
+      assert.equal(validFromIndex, 0)
+    })
+
+    it("Generic standing sell order as replacement of current batch(2 TX)", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await instance.placeStandingSellOrder([0,0], [0,1], [3,1], [3,1], { from: user_1 })
+      
+      await instance.placeStandingSellOrder([0,0], [0,1], [3,1], [3,0], { from: user_1 })
+
+      const userId = await instance.publicKeyToAccountMap.call(user_1)
+      const pointer = await instance.getStandingOrderCounter.call(userId)
+      assert.equal(pointer, 0)
+
+      const validFromIndex = await instance.getStandingOrderValidFrom(userId, pointer)
+      assert.equal(validFromIndex, 0)
+
+      const validToAuctionIndex = await instance.getStandingOrderValidFrom(userId, pointer - 1)
+      assert.equal(validToAuctionIndex, 0)
+    })
+    
+    it("Generic standing sell order as new submission ", async () => {
+      const instance = await SnappAuction.new()
+      await setupEnvironment(MintableERC20, instance, token_owner, [user_1], 2)
+
+      await instance.placeStandingSellOrder([0,0], [0,1], [3,1], [3,1], { from: user_1 })
+      
+      // Wait for current order slot to be inactive
+      await waitForNSeconds(181)
+
+      await instance.placeStandingSellOrder([0,0], [0,1], [3,1], [3,0], { from: user_1 })
+
+      const userId = await instance.publicKeyToAccountMap.call(user_1)
+      const pointer = await instance.getStandingOrderCounter.call(userId)
+      assert.equal(pointer, 1)
+
+      const validFromIndex = await instance.getStandingOrderValidFrom(userId, pointer)
+      assert.equal(validFromIndex, 1)
+
+      const validToAuctionIndex = await instance.getStandingOrderValidTo(userId, pointer - 1)
+      assert.equal(validToAuctionIndex, 0)
+    })
+  })
   describe("applyAuction()", () => {
     const new_state = "0x1"
 
