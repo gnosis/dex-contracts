25a6803299001a4a375d06c38994a87e55e1a476
Ben Smith <bh2smith@gmail.com>
2019-06-07 14:40:54 +0200

    try excluding in rules.

--- contracts/SnappBase.sol
+++ contracts/SnappBase.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.0;
 
-// solium-disable security/no-block-members
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./libraries/Merkle.sol";
