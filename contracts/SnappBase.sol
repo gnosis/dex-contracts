e23b6d71f8eeaa888095dfa880857e7fde952030
Ben Smith <bh2smith@gmail.com>
2019-06-09 23:14:37 +0200

    [hotfix] correct path of contract import

--- contracts/SnappBase.sol
+++ contracts/SnappBase.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./libraries/Merkle.sol";
 import "./libraries/IdToAddressBiMap.sol";
 
