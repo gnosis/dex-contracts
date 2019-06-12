561b149449f208769c67fcc190eda83be49afa88
josojo <josojo@hotmail.de>
2019-06-12 16:08:29 +0200

    fixing all tests

--- contracts/SnappAuction.sol
+++ contracts/SnappAuction.sol
@@ -119,7 +119,7 @@ contract SnappAuction is SnappBase {
         }
         bytes32 orderHash;
         for (uint i = 0; i < numOrders; i++) {
-            orderhash = calculateNextOrderHashIteration(packedOrders.slice(26*i, 26), orderhash);
+            orderHash = calculateNextOrderHashIteration(packedOrders.slice(26*i, 26), orderHash, accountId);
         }
         uint currentBatchIndex = standingOrders[accountId].currentBatchIndex;
         StandingOrderBatch memory currentOrderBatch = standingOrders[accountId].reservedAccountOrders[currentBatchIndex];
@@ -159,7 +159,8 @@ contract SnappAuction is SnappBase {
             createNewPendingBatchIfNecessary();
             bytes32 nextAuctionHash = calculateNextOrderHashIteration(
                 packedOrders.slice(26*i, 26),
-                auctions[auctionIndex].shaHash
+                auctions[auctionIndex].shaHash,
+                accountId
             );
             // TODO - auctions.shaHash should only need to be updated once (per index) on the outside of this loop
             auctions[auctionIndex].shaHash = nextAuctionHash;
@@ -227,21 +228,22 @@ contract SnappAuction is SnappBase {
         return bytes32(uint(accountId) + (uint(buyToken) << 16) + (uint(sellToken) << 24) + (uint(sellAmount) << 32) + (uint(buyAmount) << 128));
     }
 
-    function calculateNextOrderHashIteration(bytes orderData, bytes32 previousHash) internal
+    function calculateNextOrderHashIteration(bytes memory orderData, bytes32 previousHash, uint16 accountId) internal
         returns(bytes32)
     {
-        buyToken = BytesLib.toUint8(orderData, 0);
-        sellToken = BytesLib.toUint8(orderData, 1);
+        uint96 buyAmount;
+        assembly {  // solhint-disable no-inline-assembly
+            buyAmount := mload(add(add(orderData, 0xc), 0))
+        }
+        uint96 sellAmount;
+        assembly {  // solhint-disable no-inline-assembly
+            sellAmount := mload(add(add(orderData, 0xc), 12))
+        }
 
-        buyAmount;
-        assembly {  // solhint-disable no-inline-assembly
-            buyAmount := mload(add(add(orderData, 0xc), 2))
-        }
-        sellAmount;
-        assembly {  // solhint-disable no-inline-assembly
-            sellAmount := mload(add(add(orderData, 0xc), 14))
-        }
-        return nextHash = sha256(
+        uint8 sellToken = BytesLib.toUint8(orderData, 24);
+        uint8 buyToken = BytesLib.toUint8(orderData, 25);
+
+        return sha256(
             abi.encodePacked(
                     previousHash,  // TODO - Below todo will affect this.
                     encodeOrder(accountId, buyToken, sellToken, buyAmount, sellAmount)
