a6583bfd5bfb1931055ced436aedccb83428be59
Ben Smith <bh2smith@gmail.com>
2019-06-12 11:52:59 +0200

    now that sell and buy Amount are uint96 by default no need to check sizes

--- contracts/SnappAuction.sol
+++ contracts/SnappAuction.sol
@@ -255,10 +255,6 @@ contract SnappAuction is SnappBase {
     )
         internal view returns (bytes32)
     {
-        // Restrict buy and sell amount to occupy at most 96 bits.
-        require(buyAmount < 0x1000000000000000000000000, "Buy amount too large!");
-        require(sellAmount < 0x1000000000000000000000000, "Sell amount too large!");
-
         // Must have 0 <= tokenId < MAX_TOKENS anyway, so may as well ensure registered.
         require(buyToken < coreData.numTokens, "Buy token is not registered");
         require(sellToken < coreData.numTokens, "Sell token is not registered");
