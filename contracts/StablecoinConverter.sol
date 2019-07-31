cbb845777b48c2b1435ea0ae278649bf7a02293d
josojo <josojo@hotmail.de>
2019-07-31 14:25:26 +0200

    frist structure of orders

--- contracts/StablecoinConverter.sol
+++ contracts/StablecoinConverter.sol
@@ -2,12 +2,15 @@ pragma solidity ^0.5.0;
 
 import "./IntervalTokenStore.sol";
 import "./libraries/IdToAddressBiMap.sol";
+import "solidity-bytes-utils/contracts/BytesLib.sol";
 
 
 contract StablecoinConverter is IntervalTokenStore {
     using SafeMath for uint;
+    using BytesLib for bytes32;
 
-    mapping(address => bytes256[]) public orders;
+
+    mapping(address => bytes32[]) public orders;
     // an order has the fields
     // (buyToken (16 bit), sellToken (16 bit), buySellOrderFlag (8 bit, for easier encoding),
     // validTill (32 bit), validFrom (32 bit), buyAmount (64 bit) , sellAmount (64 bit))
@@ -15,19 +18,40 @@ contract StablecoinConverter is IntervalTokenStore {
     // validityIndicators 32 bit -> exchange can run for 41425.2246914 years
     // Amounts 64 bits, -> each number is represented as amount * 10^(9): We can represent all numbers up to 1.8446744e+28
 
-    function placeOrder(bytes256 orderbytes) public {
+    function placeOrder(bytes32 orderbytes) public {
         orders[msg.sender].push(orderbytes);
     }
 
-    function modifyOrder(bytes256 orderbytes, uint index) public {
+    function cancelOrder(uint index) public {
+        bytes32 currentOrder = orders[msg.sender][index];
+        // set validTill in currrentOrder = 0
+        bytes32 updatedOrder = bytes32(uint(currentOrder) + (uint(currentStateIndex) << 24));
+        orders[msg.sender][index] = updatedOrder;
+    }
+
+    function modifyOrder(bytes32 orderbytes, uint index) public {
+        require(orderIsExpired(msg.sender, index), "validTill not yet expired");
         orders[msg.sender][index] = orderbytes;
     }
 
     function deleteOrder(uint index) public {
+        require(orderIsExpired(msg.sender, index), "validTill not yet expired");
         delete orders[msg.sender][index];
     }
 
-    function decodeOrder(bytes memory orderData) internal pure
+    function orderIsExpired(address user, uint index) public view returns (bool){
+        address a;
+        address b;
+        bool c;
+        uint32 d;
+        uint256 e;
+        uint256 f;
+        uint32 validTill;
+        (a,b,c,validTill,d,e,f) = decodeOrder(abi.encodePacked(orders[user][index]));
+        return validTill < currentStateIndex;
+    }
+
+    function decodeOrder(bytes memory orderData) internal view
         returns(
             address buyToken,
             address sellToken,
@@ -44,19 +68,19 @@ contract StablecoinConverter is IntervalTokenStore {
         sellAmount *= 1000000000; //orders do not consider last 9 digits
         validFrom = BytesLib.toUint16(orderData, 10);
         validTill = BytesLib.toUint16(orderData, 14);
-        buySellOrderFlagUint8 = BytesLib.toUint8(orderData, 18);
-        if( buySellOrderFlagUint > 0){
+        uint8 buySellOrderFlagUint8 = BytesLib.toUint8(orderData, 18);
+        if( buySellOrderFlagUint8 > 0){
             buySellOrderFlag = true;
         }
-        sellTokenId = BytesLib.toUint16(orderData, 19);
+        uint16 sellTokenId = BytesLib.toUint16(orderData, 19);
         sellToken = tokenIdToAddressMap(sellTokenId);
-        buyTokenId = BytesLib.toUint16(orderData, 21);
+        uint16 buyTokenId = BytesLib.toUint16(orderData, 21);
         buyToken = tokenIdToAddressMap(buyTokenId);
     }
 
-    uint256 public numTokens = 0;
+    uint16 public numTokens = 0;
     IdToAddressBiMap.Data registeredTokens;
-    uint8 public constant MAX_TOKENS = 65535;
+    uint16 public constant MAX_TOKENS = 65535;
 
     function addToken( address _tokenAddress) public {
         require(numTokens + 1 <= MAX_TOKENS, "Max tokens reached");
