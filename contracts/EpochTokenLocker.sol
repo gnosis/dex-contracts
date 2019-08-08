73c5ee8a1a69d949d6eba15c1add3e8b4d45fa0f
josojo <josojo@hotmail.de>
2019-08-08 09:29:53 +0200

    fix addBalance and substractBalance

--- contracts/EpochTokenLocker.sol
+++ contracts/EpochTokenLocker.sol
@@ -118,12 +118,12 @@ contract EpochTokenLocker {
      * internal functions
      */
     function addBalance(address user, address token, uint amount) internal {
-        updateDepositsBalance(msg.sender, token);
+        updateDepositsBalance(user, token);
         balanceStates[user][token].balance = balanceStates[user][token].balance.add(amount);
     }
 
     function substractBalance(address user, address token, uint amount) internal {
-        updateDepositsBalance(msg.sender, token);
+        updateDepositsBalance(user, token);
         balanceStates[user][token].balance = balanceStates[user][token].balance.sub(amount);
     }
 
