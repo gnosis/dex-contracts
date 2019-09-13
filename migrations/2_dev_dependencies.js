29c43b3284e0e526205f33a4432d40e764aa308c
josojo <josojo@hotmail.de>
2019-09-13 15:46:16 +0200

    on my way

--- migrations/2_dev_dependencies.js
+++ migrations/2_dev_dependencies.js
@@ -1,7 +1,21 @@
-/*eslint no-undef: "off"*/
 
-const Dependencies = artifacts.require("./DevDependencies.sol")
+// /* global artifacts, web3 */
+// /* eslint no-undef: "error" */
+// const migrateDependencies = require("../src/migration_scripts_stablecoinConverter/migrate_dependencies")
+// const Dependencies = artifacts.require("./DevDependencies.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(Dependencies)
-}
+// module.exports = function (deployer, network, accounts) {
+//   if (!process.env.DEPLOY_ONLY_SNAPP_AUCTION) {
+//     migrateDependencies({
+//       artifacts,
+//       deployer,
+//       network,
+//       accounts,
+//       web3
+//     })
+//   }
+//   if (!process.env.DEPLOY_ONLY_STABLECOIN_CONVERTER) {
+//     deployer.deploy(Dependencies)
+//   }
+//   return
+// }
