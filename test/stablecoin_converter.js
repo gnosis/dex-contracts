99f3bbf190271dc44496badc30e4ebc2924bdea6
Ben Smith <bh2smith@gmail.com>
2019-10-28 12:37:23 +0100

    fix one test.

--- test/stablecoin_converter.js
+++ test/stablecoin_converter.js
@@ -435,10 +435,26 @@ contract("StablecoinConverter", async (accounts) => {
       const volume2 = [2000, feeSubtracted(2000) * 2]
       await stablecoinConverter.submitSolution(batchIndex + 1, owner, orderId, volume2, prices, tokenIdsForPrice, { from: solutionSubmitter })
 
-      assert.equal((await stablecoinConverter.getBalance.call(user_1, feeToken.address)).toNumber(), basicTrade.deposits[0].amount - getSellVolume(volume[0] - volume2[0], prices[0], prices[1]), "Sold tokens were not adjusted correctly")
-      assert.equal((await stablecoinConverter.getBalance.call(user_1, erc20_2.address)).toNumber(), volume[0] + volume2[0], "Bought tokens were not adjusted correctly")
-      assert.equal((await stablecoinConverter.getBalance.call(user_2, erc20_2.address)).toNumber(), basicTrade.deposits[1].amount - getSellVolume(volume[1] - volume2[0], prices[1], prices[0]), "Sold tokens were not adjusted correctly")
-      assert.equal((await stablecoinConverter.getBalance.call(user_2, feeToken.address)).toNumber(), volume[1] + volume2[1], "Bought tokens were not adjusted correctly")
+      assert.equal(
+        basicTrade.deposits[0].amount - getSellVolume(volume[0] + volume2[0], prices[0], prices[1]),
+        (await stablecoinConverter.getBalance.call(user_1, feeToken.address)).toNumber(),
+        "Sold tokens were not adjusted correctly"
+      )
+      assert.equal(
+        volume[0] + volume2[0],
+        (await stablecoinConverter.getBalance.call(user_1, erc20_2.address)).toNumber(),
+        "Bought tokens were not adjusted correctly"
+      )
+      assert.equal(
+        basicTrade.deposits[1].amount - getSellVolume(volume[1] + volume2[1], prices[1], prices[0]),
+        (await stablecoinConverter.getBalance.call(user_2, erc20_2.address)).toNumber(),
+        "Sold tokens were not adjusted correctly"
+      )
+      assert.equal(
+        volume[1] + volume2[1],
+        (await stablecoinConverter.getBalance.call(user_2, feeToken.address)).toNumber(),
+        "Bought tokens were not adjusted correctly"
+      )
     })
     it("settles a ring trade between 3 tokens", async () => {
       const feeToken = await MockContract.new()
