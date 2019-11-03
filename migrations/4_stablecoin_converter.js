ed1efaad8efe9cd10ce14245325a6463e6cdbe68
Ben Smith <bh2smith@gmail.com>
2019-11-03 09:07:37 +0100

    remove unnecessary lint ignore.

--- migrations/4_stablecoin_converter.js
+++ migrations/4_stablecoin_converter.js
@@ -1,5 +1,3 @@
-/*eslint no-undef: "off"*/
-
 const argv = require("../src/migration_utilities")
 const migrateStablecoinConverter = require("../src/migration_scripts_stablecoinConverter/migrate_PoC_dfusion")
 
