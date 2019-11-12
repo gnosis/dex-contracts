f84817a8a709bc22d2d92f683c86049aa6fb45cc
josojo <josojo@hotmail.de>
2019-11-12 11:10:47 +0100

    referencing to master branch token list

--- scripts/stablex/add_token_list.js
+++ scripts/stablex/add_token_list.js
@@ -3,8 +3,7 @@ const fetch = require("node-fetch")
 
 module.exports = async function (callback) {
   try {
-    //TODO(fleupold) use master branch here
-    const token_list = await (await fetch("https://raw.githubusercontent.com/gnosis/dex-react/899f14e43814446dc96e5696b85d38e639d81fdc/src/api/tokenList/tokenList.json")).json()
+    const token_list = await (await fetch("https://raw.githubusercontent.com/gnosis/dex-react/develop/src/api/tokenList/tokenList.json")).json()
     const network_id = String(await web3.eth.net.getId())
 
     const addresses = []
