3b944c431a34939f1d9f40382bd99f1a24a04c82
Ben Smith <bh2smith@gmail.com>
2020-01-01 14:38:21 +0100

    Start putting together tokenData fetch options!

--- scripts/stablex/place_margin_orders.js
+++ scripts/stablex/place_margin_orders.js
@@ -1,7 +1,65 @@
 const BatchExchange = artifacts.require("BatchExchange")
 const ERC20 = artifacts.require("ERC20")
-const { sendTxAndGetReturnValue } = require("../../test/utilities.js")
+const fetch = require("node-fetch")
 const BN = require("bn.js")
+const { sendTxAndGetReturnValue } = require("../../test/utilities.js")
+
+const token_list_url = "https://raw.githubusercontent.com/gnosis/dex-js/master/src/tokenList.json"
+
+
+const fetchTokenInfo = function(tokenIds) {
+  // console.log(`Recovering token data from URL ${token_list_url}`)
+  // const token_list = await (await fetch(token_list_url)).json()
+  // // TODO - automate this with tokenIdToAddress dot decimals
+  // const token_data = {}
+  // for (const token of token_list) {
+  //   token_data[token.id] = token.decimals
+  // }
+  // console.log(TOKEN_DECMIALS)
+
+  // console.log("Recovering token data from EVM")
+  // const tokenObjects = []
+  // for (const id of trusted_tokens) {
+  //   const tokenAddress = await instance.tokenIdToAddressMap(id)
+  //   const tokenInstance = await ERC20.at(tokenAddress)
+  //   console.log(await tokenInstance.decimals)
+  //   tokenObjects.push(await ERC20.at(tokenAddress))
+  // }
+
+  console.log("Using hardcoded token data")
+  return {
+    2: { 
+      decimals: 6,
+      name: "USDT",
+      address: 0xdac17f958d2ee523a2206206994597c13d831ec7,
+    },
+    3: { 
+      decimals: 18,
+      name: "TUSD",
+      address: 0x0000000000085d4780B73119b644AE5ecd22b376,
+    },
+    4: {
+      decimals: 6,
+      name: "USDC",
+      address: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
+    },
+    5: {
+      decimals: 18,
+      name: "PAX",
+      address: 0x8e870d67f660d95d5be530380d0ec0bd388289e1,
+    },
+    6: {
+      decimals: 2,
+      name: "GUSD",
+      address: 0x056fd409e1d7a124bd7017459dfea2f387b6d5cd,
+    },
+    7: {
+      decimals: 18,
+      name: "DAI",
+      address: 0x6b175474e89094c44da98b954eedeac495271d0f
+    },
+  }
+}
 
 const argv = require("yargs")
   .option("tokens", {
@@ -29,16 +87,6 @@ const argv = require("yargs")
   .help("Make sure that you have an RPC connection to the network in consideration")
   .version(false).argv
 
-// TODO - automate this with tokenIdToAddress dot decimals
-const TOKEN_DECMIALS = {
-  2: 6,
-  3: 18,
-  4: 6,
-  5: 18,
-  6: 2,
-  7: 18,
-}
-
 module.exports = async callback => {
   try {
     const instance = await BatchExchange.deployed()
@@ -48,14 +96,7 @@ module.exports = async callback => {
     const batch_index = (await instance.getCurrentBatchId.call()).toNumber()
 
     const trusted_tokens = argv.tokens.split(",").map(t => parseInt(t))
-    // console.log("Recovering token data...")
-    // const tokenObjects = []
-    // for (const id of trusted_tokens) {
-    //   const tokenAddress = await instance.tokenIdToAddressMap(id)
-    //   const tokenInstance = await ERC20.at(tokenAddress)
-    //   console.log(await tokenInstance.decimals)
-    //   tokenObjects.push(await ERC20.at(tokenAddress))
-    // }
+    const token_data = fetchTokenInfo(trusted_tokens)
 
     const expectedReturnFactor = 1 + argv.margin / 100
     const sellAmount = argv.sellAmount
@@ -67,16 +108,16 @@ module.exports = async callback => {
     let sellAmounts = []
     for (let i = 0; i < trusted_tokens.length - 1; i++) {
       const tokenA = trusted_tokens[i]
-      const tokenScaleA = new BN(10).pow(new BN(TOKEN_DECMIALS[tokenA]))
+      const tokenScaleA = new BN(10).pow(new BN(token_data[tokenA].decimals))
       for (let j = i + 1; j < trusted_tokens.length; j++) {
         const tokenB = trusted_tokens[j]
-        const tokenScaleB = new BN(10).pow(new BN(TOKEN_DECMIALS[tokenB]))
+        const tokenScaleB = new BN(10).pow(new BN(token_data[tokenB].decimals))
         buyTokens = buyTokens.concat(tokenA, tokenB)
         sellTokens = sellTokens.concat(tokenB, tokenA)
         buyAmounts = buyAmounts.concat(tokenScaleA.muln(buyAmount), tokenScaleB.muln(buyAmount))
         sellAmounts = sellAmounts.concat(tokenScaleB.muln(sellAmount), tokenScaleA.muln(sellAmount))
-        console.log(`Selling ${sellAmounts[sellAmounts.length - 2]} of token ${tokenA} for ${buyAmounts[buyAmounts.length - 2]} of token ${tokenB}`)
-        console.log(`Selling ${sellAmounts[sellAmounts.length - 1]} of token ${tokenB} for ${buyAmounts[buyAmounts.length - 1]} of token ${tokenA}`)
+        console.log(`Selling ${sellAmounts[sellAmounts.length - 2]} ${token_data[tokenB].name} for ${buyAmounts[buyAmounts.length - 2]} ${token_data[tokenA].name}`)
+        console.log(`Selling ${sellAmounts[sellAmounts.length - 1]} ${token_data[tokenA].name} for ${buyAmounts[buyAmounts.length - 1]} ${token_data[tokenB].name}`)
       }
     }
 
