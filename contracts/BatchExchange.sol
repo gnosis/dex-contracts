384fa148e27520f9d4943b419bc74f7870464a12
Ben Smith <bh2smith@gmail.com>
2020-01-06 15:34:11 +0100

    orderId's (although corresponding to the index of an array) are restriced to u16

--- contracts/BatchExchange.sol
+++ contracts/BatchExchange.sol
@@ -104,11 +104,11 @@ contract BatchExchange is EpochTokenLocker {
      * currently being solved. It remains in storage but will not be tradable in any future
      * batch to be solved.
      */
-    event OrderCancelation(address indexed owner, uint256 id);
+    event OrderCancelation(address indexed owner, uint16 id);
 
     /** @dev Event emitted when an order is removed from storage.
      */
-    event OrderDeletion(address indexed owner, uint256 id);
+    event OrderDeletion(address indexed owner, uint16 id);
 
     /** @dev Event emitted when a new trade is settled
      */
