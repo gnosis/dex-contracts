2f34131e43becd8283c4e4bb803638687f5b2a76
Ben Smith <bh2smith@gmail.com>
2020-01-21 16:27:23 +0100

    use coverage network.

--- .solcover.js
+++ .solcover.js
@@ -5,7 +5,6 @@ module.exports = {
   providerOptions: {
     default_balance_ether: 500000000,
   },
-  testrpcOptions: "-p 8555",
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true, // Run the grep's inverse set.
