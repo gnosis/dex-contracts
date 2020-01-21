17088d4aa1623470d5ac02be9e04d8f54c7332ec
Ben Smith <bh2smith@gmail.com>
2020-01-21 15:30:50 +0100

    use different port

--- .solcover.js
+++ .solcover.js
@@ -5,6 +5,7 @@ module.exports = {
   providerOptions: {
     default_balance_ether: 500000000,
   },
+  testrpcOptions: "-p 8555",
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true, // Run the grep's inverse set.
