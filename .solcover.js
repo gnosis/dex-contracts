c58f4f7ef5b7db41140b88262db577e924ef45dd
Ben Smith <bh2smith@gmail.com>
2020-01-21 12:29:32 +0100

    Skip Large example on coverage report.

--- .solcover.js
+++ .solcover.js
@@ -5,4 +5,8 @@ module.exports = {
   providerOptions: {
     default_balance_ether: 500000000,
   },
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true               // Run the grep's inverse set.
+  }
 }
