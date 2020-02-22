59bed94c519f3c776d0053ceb3884278261a3087
josojo <josojo@hotmail.de>
2020-02-22 21:36:58 +0300

    reseting urls for rinkeby and mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -39,9 +39,9 @@ module.exports = {
   ...truffleConfig({
     mnemonic,
     privateKey,
-    urlRinkeby: "https://node.rinkeby.gnosisdev.com",
+    urlRinkeby: "https://rinkeby.infura.io/v3/".concat(infuraKey),
     urlKovan: "https://kovan.infura.io/v3/".concat(infuraKey),
-    urlMainnet: "https://node.mainnet.gnosisdev.com",
+    urlMainnet: "https://mainnet.infura.io/v3/".concat(infuraKey),
     urlDevelopment,
     gasPriceGWei,
     gas,
