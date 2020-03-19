1afedb563f55eec1cdfde5462612b1185c4e51ab
Felix Leupold <felixleupold90@gmail.com>
2020-03-19 10:29:40 +0100

    using BN in Fraction

--- src/fraction.ts
+++ src/fraction.ts
@@ -1,19 +1,31 @@
-export class Fraction {
-  private numerator: number;
-  private denominator: number;
+import BN from "bn.js";
 
-  constructor(numerator: number, denominator: number) {
-    this.numerator = numerator;
-    if (denominator == 0) {
+export class Fraction {
+  private numerator: BN;
+  private denominator: BN;
+
+  constructor(numerator: BN | number, denominator: BN | number) {
+    if (typeof numerator == "number") {
+      this.numerator = new BN(numerator);
+    } else {
+      this.numerator = numerator;
+    }
+
+    if (typeof denominator == "number") {
+      this.denominator = new BN(denominator);
+    } else {
+      this.denominator = denominator;
+    }
+
+    if (this.denominator.isZero()) {
       throw Error("Denominator cannot be zero");
     }
-    this.denominator = denominator;
   }
 
   reduce() {
     const greatest_common_denominator = gcd(this.numerator, this.denominator);
-    this.numerator /= greatest_common_denominator;
-    this.denominator /= greatest_common_denominator;
+    this.numerator = this.numerator.div(greatest_common_denominator);
+    this.denominator = this.denominator.div(greatest_common_denominator);
   }
 
   inverted() {
@@ -21,13 +33,13 @@ export class Fraction {
   }
 
   negated() {
-    return new Fraction(this.numerator * -1, this.denominator);
+    return new Fraction(this.numerator.neg(), this.denominator);
   }
 
   mul(other: Fraction) {
     const result = new Fraction(
-      this.numerator * other.numerator,
-      this.denominator * other.denominator
+      this.numerator.mul(other.numerator),
+      this.denominator.mul(other.denominator)
     );
     result.reduce();
     return result;
@@ -43,15 +55,17 @@ export class Fraction {
 
   add(other: Fraction) {
     const result = new Fraction(
-      this.numerator * other.denominator + other.numerator * this.denominator,
-      this.denominator * other.denominator
+      this.numerator
+        .mul(other.denominator)
+        .add(other.numerator.mul(this.denominator)),
+      this.denominator.mul(other.denominator)
     );
     result.reduce();
     return result;
   }
 
   toNumber() {
-    return this.numerator / this.denominator;
+    return this.numerator.toNumber() / this.denominator.toNumber();
   }
 
   toJSON() {
@@ -60,15 +74,15 @@ export class Fraction {
 }
 
 // https://github.com/AllAlgorithms/typescript/blob/master/math/gcd/gcd.ts
-function gcd(num1: number, num2: number): number {
-  if (num1 === 0 || num2 === 0) {
-    return 0;
+function gcd(num1: BN, num2: BN): BN {
+  if (num1.isZero() || num2.isZero()) {
+    return new BN(0);
   }
-  if (num1 === num2) {
+  if (num1.eq(num2)) {
     return num1;
   }
-  if (num1 > num2) {
-    return gcd(num1 - num2, num2);
+  if (num1.gt(num2)) {
+    return gcd(num1.sub(num2), num2);
   }
-  return gcd(num1, num2 - num1);
+  return gcd(num1, num2.sub(num1));
 }
