e13cc7666d0fe3d8e436ef4759214bbf4f0bf3b6
Felix Leupold <felixleupold90@gmail.com>
2020-03-25 17:22:49 +0100

    adjust return type

--- src/index.d.ts
+++ src/index.d.ts
@@ -87,4 +87,4 @@ export declare function decodeOrdersBN(bytes: string): OrderBN[];
 export declare function getOpenOrdersPaginated(
   contract: BatchExchangeViewer,
   pageSize: number
-): Iterator<OrderBN[]>;
+): Iterator<Promise<OrderBN[]>>;
