79b04e342da03afcfe6cd948d4c0d7ac523044a5
Felix Leupold <felixleupold90@gmail.com>
2020-03-25 12:59:10 +0100

    export js files

--- src/index.js
+++ src/index.js
@@ -10,5 +10,7 @@ module.exports = {
   BatchExchange: require("../build/contracts/BatchExchange.json"),
   BatchExchangeViewer: require("../build/contracts/BatchExchangeViewer.json"),
   SnappAuction: require("../build/contracts/SnappAuction.json"),
+  ...require("../typescript/common/fraction.js"),
+  ...require("../typescript/common/orderbook.js"),
   ...require("./encoding.js"),
 }
