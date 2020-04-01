a3341851812cb2414961622def8a610cc9b4be7c
Felix Leupold <felixleupold90@gmail.com>
2020-04-01 09:35:05 +0200

    remove multicall

--- contracts/DevDependencies.sol
+++ contracts/DevDependencies.sol
@@ -7,5 +7,4 @@ pragma solidity ^0.5.0;
 //  For other environments, only use compiled contracts from the NPM package.
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-import "solidity-multicall/contracts/MultiCaller.sol";
 import "@gnosis.pm/owl-token/contracts/TokenOWLProxy.sol";
