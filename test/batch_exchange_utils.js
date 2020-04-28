3116b9d3856dd009f2db9314aa084192c7f317af
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2020-04-28 12:32:31 +0200

    Test getWithdrawableAmount

--- test/batch_exchange_utils.js
+++ test/batch_exchange_utils.js
@@ -1,6 +1,8 @@
 const MockContract = artifacts.require("MockContract")
 
-const { closeAuction, getBalanceState } = require("../scripts/utilities.js")
+const BN = require("bn.js")
+
+const { closeAuction, getBalanceState, getWithdrawableAmount } = require("../scripts/utilities.js")
 
 const { setupGenericStableX } = require("./stablex_utils")
 
@@ -20,5 +22,71 @@ contract("BatchExchange utils", async (accounts) => {
       const balance = await getBalanceState(accounts[0], erc20.address, batchExchange.address, web3)
       assert.equal(balance.toString(16), "100")
     })
+  })
+
+  describe("getWithdrawableAmount()", async () => {
+    const startingAmount = new BN("100")
+
+    const setup = async function () {
+      const batchExchange = await setupGenericStableX()
+      await closeAuction(batchExchange)
+      const erc20 = await MockContract.new()
+      await erc20.givenAnyReturnBool(true)
+      await batchExchange.deposit(erc20.address, startingAmount)
+      await closeAuction(batchExchange)
+      // force balance update with an empty withdraw
+      await batchExchange.withdraw(accounts[0], erc20.address)
+      // state of the contract after the execution of this function:
+      // balanceState[accounts[0]][erc20.address] =
+      // {
+      //   balance: startingAmount,
+      //   pendingDeposits: null,
+      //   pendingWithdraws: null,
+      // }
+      return [batchExchange, erc20]
+    }
+
+    it("returns zero for future withdraw request", async () => {
+      const [batchExchange, erc20] = await setup()
+      const futureBatchId = 2 ** 32 - 1
+      await batchExchange.requestFutureWithdraw(erc20.address, startingAmount, futureBatchId)
+      const withdrawableAmount = await getWithdrawableAmount(accounts[0], erc20.address, batchExchange, web3)
+      assert.equal(withdrawableAmount.toString(), "0")
+    })
+    it("returns amount when withdraw was already requested", async () => {
+      const [batchExchange, erc20] = await setup()
+      await batchExchange.requestWithdraw(erc20.address, startingAmount)
+      await closeAuction(batchExchange)
+      const withdrawableAmount = await getWithdrawableAmount(accounts[0], erc20.address, batchExchange, web3)
+      assert.equal(withdrawableAmount.toString(), startingAmount.toString())
+    })
+    it("returns entire balance when withdraw of more than available was requested", async () => {
+      const [batchExchange, erc20] = await setup()
+      await batchExchange.requestWithdraw(erc20.address, startingAmount.addn(1))
+      await closeAuction(batchExchange)
+      const withdrawableAmount = await getWithdrawableAmount(accounts[0], erc20.address, batchExchange, web3)
+      assert.equal(withdrawableAmount.toString(), startingAmount.toString())
+    })
+    it("ignores deposits if funds are not yet available", async () => {
+      const [batchExchange, erc20] = await setup()
+      const newAmount = new BN("200")
+      const fullBalance = newAmount.add(startingAmount)
+      await batchExchange.requestWithdraw(erc20.address, fullBalance)
+      await closeAuction(batchExchange)
+      await batchExchange.deposit(erc20.address, newAmount)
+      const withdrawableAmount = await getWithdrawableAmount(accounts[0], erc20.address, batchExchange, web3)
+      assert.equal(withdrawableAmount.toString(), startingAmount.toString())
+    })
+    it("considers deposits if funds are available", async () => {
+      const [batchExchange, erc20] = await setup()
+      const newAmount = new BN("200")
+      const fullBalance = newAmount.add(startingAmount)
+      await batchExchange.requestWithdraw(erc20.address, fullBalance)
+      await batchExchange.deposit(erc20.address, newAmount)
+      await closeAuction(batchExchange)
+      const withdrawableAmount = await getWithdrawableAmount(accounts[0], erc20.address, batchExchange, web3)
+      assert.equal(withdrawableAmount.toString(), fullBalance.toString())
+    })
+    // untested case: lastCreditBatchId != 0
   })
 })
