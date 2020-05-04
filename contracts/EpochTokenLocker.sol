f542b35534d996679428cec951ac63a56e5c757a
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-05-04 10:22:36 +0200

    Use new openzeppelin package

--- contracts/EpochTokenLocker.sol
+++ contracts/EpochTokenLocker.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "openzeppelin-solidity/contracts/math/Math.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 /** @title Epoch Token Locker
