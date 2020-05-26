25c97074442c79627fa5c498aa1b531dc381b4a4
Ben Smith <bh2smith@gmail.com>
2020-05-26 15:32:20 +0200

    I'm getting good at this!

---
+++ test/LargeExamples.spec.ts
@@ -0,0 +1,108 @@
+import { solutionSubmissionParams, largeRing30 } from "./resources/examples";
+import {
+  closeAuction,
+  makeDeposits,
+  placeOrders,
+  setupGenericStableX,
+} from "./utilities";
+
+contract("BatchExchange", async (accounts) => {
+  const solver = accounts.pop();
+  const competingSolver = accounts.pop();
+
+  describe("Large Examples [ @skip-on-coverage ]", () => {
+    it("ensures hard gas limit on largest possible ring trade ", async () => {
+      const batchExchange = await setupGenericStableX(30);
+      const limit = 6e8;
+
+      const tradeExample = largeRing30;
+      // Deposit double sufficient amount and immediately request withdraw for half
+      await makeDeposits(batchExchange, accounts, tradeExample.deposits);
+      for (const order of tradeExample.orders) {
+        const tokenAddress = await batchExchange.tokenIdToAddressMap(
+          order.buyToken,
+        );
+        await batchExchange.requestWithdraw(tokenAddress, order.buyAmount, {
+          from: accounts[order.user],
+        });
+      }
+      await closeAuction(batchExchange);
+
+      const batchId = (await batchExchange.getCurrentBatchId()).toNumber();
+      const orderIds = await placeOrders(
+        batchExchange,
+        accounts,
+        tradeExample.orders,
+        batchId + 1,
+      );
+      await closeAuction(batchExchange);
+
+      // Ensure that first 30 orders have valid withdraw requests.
+      for (const order of tradeExample.orders.slice(0, 30)) {
+        const tokenAddress = await batchExchange.tokenIdToAddressMap(
+          order.buyToken,
+        );
+        assert(
+          await batchExchange.hasValidWithdrawRequest(
+            accounts[order.user],
+            tokenAddress,
+          ),
+          "Expected valid withdraw requests before first solution submission.",
+        );
+      }
+
+      const solution = solutionSubmissionParams(
+        tradeExample.solutions[0],
+        accounts,
+        orderIds,
+      );
+      const firstSubmissionTX = await batchExchange.submitSolution(
+        batchId,
+        solution.objectiveValue,
+        solution.owners,
+        solution.touchedorderIds,
+        solution.volumes,
+        solution.prices,
+        solution.tokenIdsForPrice,
+        { from: solver },
+      );
+      assert(
+        firstSubmissionTX.receipt.gasUsed < limit,
+        `Solution submission exceeded gas limit at ${firstSubmissionTX.receipt.gasUsed}`,
+      );
+
+      // Ensure second 30 order's users valid withdraw requests.
+      for (const order of tradeExample.orders.slice(30)) {
+        const tokenAddress = await batchExchange.tokenIdToAddressMap(
+          order.buyToken,
+        );
+        assert(
+          await batchExchange.hasValidWithdrawRequest(
+            accounts[order.user],
+            tokenAddress,
+          ),
+        );
+      }
+
+      const solution2 = solutionSubmissionParams(
+        tradeExample.solutions[1],
+        accounts,
+        orderIds,
+      );
+      const secondSubmissionTX = await batchExchange.submitSolution(
+        batchId,
+        solution2.objectiveValue,
+        solution2.owners,
+        solution2.touchedorderIds,
+        solution2.volumes,
+        solution2.prices,
+        solution2.tokenIdsForPrice,
+        { from: competingSolver },
+      );
+      assert(
+        secondSubmissionTX.receipt.gasUsed < limit,
+        `Competing solution submission exceeded gas limit at ${secondSubmissionTX.receipt.gasUsed}`,
+      );
+    });
+  });
+});
