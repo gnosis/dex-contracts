d8d79a7f2d0c93a890786ea4ebe7090bf162a667
Ben Smith <bh2smith@gmail.com>
2020-05-27 12:53:23 +0200

    include log statements in deposit script and delete JS file.

--- scripts/deposit.ts
+++ scripts/deposit.ts
@@ -1,3 +1,5 @@
+import BN from "bn.js";
+import { factory } from "../ConfigLog4";
 const BatchExchange = artifacts.require("BatchExchange");
 const ERC20 = artifacts.require("ERC20.sol");
 const argv = require("yargs")
@@ -15,42 +17,43 @@ const argv = require("yargs")
   .version(false).argv;
 
 const zero_address = "0x0000000000000000000000000000000000000000";
+const log = factory.getLogger("deposit");
 
 module.exports = async (callback: Truffle.ScriptCallback) => {
   try {
-    const amount = web3.utils.toWei(String(argv.amount));
+    // Is is possible that this does not account for token.decimals?
+    const amount = new BN(web3.utils.toWei(String(argv.amount)));
     const instance = await BatchExchange.deployed();
     const accounts = await web3.eth.getAccounts();
     const depositor = await accounts[argv.accountId];
-
+    log.info(`Using account ${depositor}`);
     const token_address = await instance.tokenIdToAddressMap(argv.tokenId);
-    if (token_address == zero_address) {
+    if (token_address === zero_address) {
       callback(`Error: No token registered at index ${argv.tokenId}`);
     }
-
     const token = await ERC20.at(token_address);
-    const depositor_balance = await token.balanceOf.call(depositor);
-    if (depositor_balance.lt(amount)) {
+    const depositorBalance = await token.balanceOf.call(depositor);
+    if (depositorBalance.lt(amount)) {
       callback(
-        `Error: Depositor has insufficient balance ${depositor_balance} < ${amount}.`,
+        `Error: Insufficient balance (${depositorBalance.toString()} < ${amount.toString()}).`,
       );
     }
 
-    const allowance = (
-      await token.allowance.call(depositor, instance.address)
-    ).toString();
-    if (allowance < amount) {
-      await token.approve(instance.address, amount, { from: depositor });
+    const allowance = await token.allowance.call(depositor, instance.address);
+    if (allowance.lt(amount)) {
+      log.info(
+        `Submitting approval for amount ${amount.sub(allowance).toString()}`,
+      );
+      await token.approve(instance.address, amount.sub(allowance), {
+        from: depositor,
+      });
     }
 
     await instance.deposit(token_address, amount, { from: depositor });
     const tradeable_at = (
       await instance.getPendingDeposit(depositor, token_address)
     )[1];
-
-    console.log(
-      `Deposit successful. Can be traded as of batch ${tradeable_at}`,
-    );
+    log.info(`Deposit successful! Can be traded as of batch ${tradeable_at}`);
     callback();
   } catch (error) {
     callback(error);
