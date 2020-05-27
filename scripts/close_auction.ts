3ca3eb692f829babf8e6c96e86d753381c3b543e
Ben Smith <bh2smith@gmail.com>
2020-05-27 14:56:38 +0200

    couldnt access networks

--- scripts/close_auction.ts
+++ scripts/close_auction.ts
@@ -2,13 +2,9 @@ import { closeAuction } from "../test/utilities";
 import { factory } from "../ConfigLog4";
 const log = factory.getLogger("scripts.close_auction");
 
+// Note that this script only works in local blockchains
 module.exports = async (callback: Truffle.ScriptCallback) => {
   try {
-    if (argv.network && argv.network != "development") {
-      log.warn(
-        "Note that this script can only be used with a local blockchain",
-      );
-    }
     const BatchExchange = artifacts.require("BatchExchange");
     const instance = await BatchExchange.deployed();
     await closeAuction(instance, web3);
