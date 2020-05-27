05066affc37e9cbc145905cbcf6f2551dd6df423
Ben Smith <bh2smith@gmail.com>
2020-05-27 19:41:33 +0200

    Migrate close_auction.js

---
+++ scripts/close_auction.ts
@@ -0,0 +1,16 @@
+import { closeAuction } from "../test/utilities";
+import { factory } from "../src/logging";
+const log = factory.getLogger("scripts.close_auction");
+
+// Note that this script only works in local blockchains
+module.exports = async (callback: Truffle.ScriptCallback) => {
+  try {
+    const BatchExchange = artifacts.require("BatchExchange");
+    const instance = await BatchExchange.deployed();
+    await closeAuction(instance, web3);
+    log.info("Auction closed");
+    callback();
+  } catch (error) {
+    callback(error);
+  }
+};
