6c047de567664b154c706644df6a1ec5e311145c
Ben Smith <bh2smith@gmail.com>
2020-05-28 09:51:55 +0200

    [JS->TS] Migrate place_order.js

---
+++ scripts/place_order.ts
@@ -0,0 +1,70 @@
+import { sendTxAndGetReturnValue } from "../test/utilities";
+import { factory } from "../src/logging";
+const log = factory.getLogger("scripts.deposit");
+
+const BatchExchange = artifacts.require("BatchExchange");
+
+const argv = require("yargs")
+  .option("accountId", {
+    describe: "Account index of the order placer",
+  })
+  .option("sellToken", {
+    describe: "Token to be sold",
+  })
+  .option("buyToken", {
+    describe: "token to be bought",
+  })
+  .option("minBuy", {
+    describe:
+      "minimum amount to be bought (in 10**18 WEI of buyToken, e.g. 1 = 1ETH)",
+  })
+  .option("maxSell", {
+    describe:
+      "minimum amount to be sold (in 10**18 WEI of sellToken, e.g. 1 = 1ETH)",
+  })
+  .option("validFor", {
+    describe: "the number of auctions for which this orders is valid",
+  })
+  .demand([
+    "accountId",
+    "sellToken",
+    "buyToken",
+    "minBuy",
+    "maxSell",
+    "validFor",
+  ])
+  .help(false)
+  .version(false).argv;
+
+module.exports = async (callback: Truffle.ScriptCallback) => {
+  try {
+    const minBuy = web3.utils.toWei(String(argv.minBuy));
+    const maxSell = web3.utils.toWei(String(argv.maxSell));
+
+    const instance = await BatchExchange.deployed();
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[argv.accountId];
+
+    const batch_index = (await instance.getCurrentBatchId()).toNumber();
+    const valid_until = batch_index + parseInt(argv.validFor);
+
+    const id = await sendTxAndGetReturnValue(
+      instance.placeOrder,
+      argv.buyToken,
+      argv.sellToken,
+      valid_until,
+      minBuy,
+      maxSell,
+      {
+        from: account,
+      },
+    );
+
+    log.info(
+      `Limit Sell Order placed ID ${id}. Valid from batch ${batch_index} until ${valid_until}`,
+    );
+    callback();
+  } catch (error) {
+    callback(error);
+  }
+};
