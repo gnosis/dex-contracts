4079562e4f942e321964e2f916d9b211406c3876
Ben Smith <bh2smith@gmail.com>
2020-05-29 15:35:55 +0200

    migrate utilities

--- scripts/util.ts
+++ scripts/util.ts
@@ -1,5 +1,9 @@
 import BN from "bn.js";
-import type { BatchExchangeInstance } from "../types/truffle-typings";
+import type {
+  BatchExchangeInstance,
+  TokenOwlInstance,
+} from "../types/truffle-typings";
+import type { SolutionSubmission } from "../test/resources/examples/model";
 import { factory } from "../src/logging";
 const log = factory.getLogger("scripts.util");
 
@@ -17,7 +21,7 @@ export async function getBatchExchange(
 
 export async function getOwl(
   artifacts: Truffle.Artifacts,
-): Promise<Truffle.ContractInstance> {
+): Promise<TokenOwlInstance> {
   const TokenOWL = artifacts.require("TokenOWL");
   const batchExchange = await getBatchExchange(artifacts);
   const owlAddress = await batchExchange.feeToken();
@@ -28,25 +32,38 @@ export async function getOwl(
 async function setAllowance(
   token: Truffle.ContractInstance,
   account: string,
-  amount: BN,
-  batchExchange: BatchExchangeInstance,
+  amount: string | BN,
+  exchange: BatchExchangeInstance,
 ) {
   log.info(
-    `Approving BatchExchange at ${batchExchange.address} for ${amount} of token ${token.address}`,
+    `Approving BatchExchange at ${exchange.address} for ${amount} of token ${token.address}`,
   );
-  await token.contract.methods.approve(batchExchange.address, amount, {
+  await token.contract.methods.approve(exchange.address, amount, {
     from: account,
   });
+}
+
+export async function setAllowances(
+  users: string[],
+  amount: string | BN,
+  exchange: BatchExchangeInstance,
+  tokens: Truffle.ContractInstance[],
+): Promise<void> {
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < tokens.length; j++) {
+      await setAllowance(tokens[j], users[i], amount, exchange);
+    }
+  }
 }
 
 export async function addTokens(
   tokenAddresses: string[],
   account: string,
-  batchExchange: BatchExchangeInstance,
+  exchange: BatchExchangeInstance,
   owl: Truffle.ContractInstance,
 ): Promise<ExchangeToken[]> {
   // Get amount of required OWL for listing all tokens
-  const feeForAddingToken = await batchExchange.FEE_FOR_LISTING_TOKEN_IN_OWL();
+  const feeForAddingToken = await exchange.FEE_FOR_LISTING_TOKEN_IN_OWL();
   const totalFees = feeForAddingToken.mul(new BN(tokenAddresses.length));
 
   // Ensure the user has enough OWL balance
@@ -58,27 +75,27 @@ export async function addTokens(
   // Set OWL allowance if necessary
   const allowanceOfOWL = await owl.contract.methods.allowance(
     account,
-    batchExchange.address,
+    exchange.address,
   );
   if (totalFees.gt(allowanceOfOWL)) {
     // TODO: Only approve the minimum required amount totalFees.sub(allowanceOfOWL)
-    await setAllowance(owl, account, totalFees, batchExchange);
+    await setAllowance(owl, account, totalFees, exchange);
   }
 
   // List all tokens (if not listed previously)
   const tokens = [];
   for (const tokenAddress of tokenAddresses) {
-    const isTokenListed = await batchExchange.hasToken(tokenAddress);
+    const isTokenListed = await exchange.hasToken(tokenAddress);
 
     if (!isTokenListed) {
-      await batchExchange.addToken(tokenAddress);
+      await exchange.addToken(tokenAddress);
       log.info(`Successfully added token ${tokenAddress}`);
     } else {
       log.info(`The token ${tokenAddress} was already added`);
     }
 
     // Get token information
-    const tokenId = await batchExchange.tokenAddressToIdMap(tokenAddress);
+    const tokenId = await exchange.tokenAddressToIdMap(tokenAddress);
     tokens.push({
       id: tokenId.toNumber(),
       address: tokenAddress,
@@ -96,4 +113,109 @@ export function parseArgs(): string[] {
     args.splice(index, 2);
   }
   return args;
+}
+
+export async function deleteOrders(
+  orderIds: number[],
+  accounts: string[],
+  exchange: BatchExchangeInstance,
+): Promise<void> {
+  log.info(`Canceling ${orderIds.length} orders for ${accounts.length} users`);
+  for (let i = 0; i < orderIds.length; i++) {
+    const orderId = orderIds[i];
+    const account = accounts[i];
+    const cancelReceipt = await exchange.cancelOrders([orderId], {
+      from: account,
+    });
+    const events = cancelReceipt.logs.map((log: any) => log.event).join(", ");
+    log.info(
+      `Canceled/Deleted order ${orderId} for user {${account}}. Emitted events: ${events}`,
+    );
+  }
+}
+
+export async function submitSolution(
+  name: string,
+  batchId: number,
+  solution: SolutionSubmission,
+  solverAddress: string,
+  exchange: BatchExchangeInstance,
+): Promise<void> {
+  log.info(`Submit "${name}":
+  - Objective value: ${solution.objectiveValue}
+  - Touched orders: ${solution.touchedorderIds.join(", ")}
+  - Volumes: ${solution.volumes.join(", ")}
+  - Prices: ${solution.prices.join(", ")}
+  - Token ids for prices: ${solution.tokenIdsForPrice.join(", ")}`);
+  const objectiveValue = await exchange.submitSolution(
+    batchId,
+    1,
+    solution.owners,
+    solution.touchedorderIds,
+    solution.volumes,
+    solution.prices,
+    solution.tokenIdsForPrice,
+    { from: solverAddress },
+  );
+  log.info(`Transaction for ${name}: ${objectiveValue.tx}`);
+}
+
+export async function getBatchId(
+  exchange: BatchExchangeInstance,
+): Promise<number> {
+  const batchId = await exchange.getCurrentBatchId();
+  return batchId.toNumber();
+}
+
+export async function createMintableToken(
+  artifacts: Truffle.Artifacts,
+): Promise<Truffle.ContractInstance> {
+  const ERC20Mintable = artifacts.require("ERC20Mintable");
+  return ERC20Mintable.new();
+}
+
+async function mintToken(
+  token: Truffle.ContractInstance,
+  account: string,
+  minter: string,
+  amount: string,
+) {
+  log.info(
+    `Mint ${amount} of token ${token.address} for user ${account}. Using ${minter} as the minter`,
+  );
+  await token.contract.methods.mint(account, amount, { from: minter });
+}
+
+export async function mintTokens(
+  tokens: Truffle.ContractInstance[],
+  users: string[],
+  minter: string,
+  amount: string,
+): Promise<void> {
+  for (let i = 0; i < tokens.length; i++) {
+    for (let j = 0; j < users.length; j++) {
+      await mintToken(tokens[i], users[j], minter, amount);
+    }
+  }
+}
+
+async function _mintOwl(
+  account: string,
+  minter: string,
+  amount: string,
+  owl: TokenOwlInstance,
+) {
+  log.info(`Mint ${amount} of OWL for user ${account}`);
+  return owl.mintOWL(account, amount, { from: minter });
+}
+
+export async function mintOwl(
+  users: string[],
+  minter: string,
+  amount: string,
+  owl: TokenOwlInstance,
+): Promise<void> {
+  for (let i = 0; i < users.length; i++) {
+    await _mintOwl(users[i], minter, amount, owl);
+  }
 }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,3 @@
-import { setAllowance } from "./util"
-
 const getOrderData = async function (instance, callback, web3, argv) {
   const minBuy = web3.utils.toWei(String(argv.minBuy))
   const maxSell = web3.utils.toWei(String(argv.maxSell))
@@ -22,91 +20,6 @@ const getOrderData = async function (instance, callback, web3, argv) {
   return [argv.buyToken, argv.sellToken, minBuy, maxSell, sender]
 }
 
-async function createMintableToken(artifacts) {
-  const ERC20Mintable = artifacts.require("ERC20Mintable")
-  return ERC20Mintable.new()
-}
-
-async function _mintOwl({ account, minter, amount, owl }) {
-  console.log("Mint %d of OWL for user %s", amount, account)
-  return owl.mintOWL(account, amount, { from: minter })
-}
-
-async function mintTokens({ tokens, minter, users, amount }) {
-  for (let i = 0; i < tokens.length; i++) {
-    for (let j = 0; j < users.length; j++) {
-      await mintToken({ token: tokens[i], account: users[j], minter, amount })
-    }
-  }
-}
-
-async function mintToken({ token, account, minter, amount }) {
-  console.log("Mint %d of token %s for user %s. Using %s as the minter", amount, token.address, account, minter)
-  await token.mint(account, amount, { from: minter })
-}
-
-async function deleteOrders({ orderIds, accounts, batchExchange }) {
-  console.log("Cancel %d orders for %d users", orderIds.length, accounts.length)
-  for (let i = 0; i < orderIds.length; i++) {
-    const orderId = orderIds[i]
-    const account = accounts[i]
-    const cancelReceipt = await batchExchange.cancelOrders([orderId], { from: account })
-    const events = cancelReceipt.logs.map((log) => log.event).join(", ")
-    console.log("Canceled/Deleted order %s for user %s. Emitted events: %s", orderId.toString(10), account, events)
-  }
-}
-
-async function getBatchId(batchExchange) {
-  const batchId = await batchExchange.getCurrentBatchId.call()
-  return batchId.toNumber()
-}
-
-async function submitSolution({ name, batchId, solution, solverAddress, batchExchange }) {
-  console.log(`Submit "${name}":
-  - Objective value: ${solution.objectiveValue}
-  - Touched orders: ${solution.touchedorderIds.join(", ")}
-  - Volumes: ${solution.volumes.join(", ")}
-  - Prices: ${solution.prices.join(", ")}
-  - Token ids for prices: ${solution.tokenIdsForPrice.join(", ")}`)
-  const objectiveValue = await batchExchange.submitSolution(
-    batchId,
-    1,
-    solution.owners,
-    solution.touchedorderIds,
-    solution.volumes,
-    solution.prices,
-    solution.tokenIdsForPrice,
-    { from: solverAddress }
-  )
-  console.log(`Transaction for ${name}: ${objectiveValue.tx}`)
-}
-
-async function setAllowances({ users, amount, batchExchange, tokens }) {
-  for (let i = 0; i < users.length; i++) {
-    for (let j = 0; j < tokens.length; j++) {
-      await setAllowance({
-        token: tokens[j],
-        account: users[i],
-        amount,
-        batchExchange,
-      })
-    }
-  }
-}
-
-async function mintOwl({ users, minter, amount, owl }) {
-  for (let i = 0; i < users.length; i++) {
-    await _mintOwl({ account: users[i], minter, amount, owl })
-  }
-}
-
 module.exports = {
   getOrderData,
-  mintOwl,
-  deleteOrders,
-  setAllowances,
-  submitSolution,
-  getBatchId,
-  createMintableToken,
-  mintTokens,
 }
