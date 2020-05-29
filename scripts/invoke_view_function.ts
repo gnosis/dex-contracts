26afadc10c7caac318ea9aff3b238d431e13f09f
Ben Smith <bh2smith@gmail.com>
2020-05-29 09:50:58 +0200

    cast info to unknown and log the output to console

--- scripts/invoke_view_function.ts
+++ scripts/invoke_view_function.ts
@@ -1,6 +1,4 @@
 import { parseArgs, getBatchExchange } from "./util";
-import { factory } from "../src/logging";
-const log = factory.getLogger("scripts.invoke_view_function");
 
 module.exports = async (callback: Truffle.ScriptCallback) => {
   try {
@@ -12,8 +10,14 @@ module.exports = async (callback: Truffle.ScriptCallback) => {
     }
     const [functionName, ...arg] = args;
     const exchange = await getBatchExchange(artifacts);
-    const info = await (exchange as any)[functionName].call(...arg);
-    log.info(info);
+    const info: unknown = await exchange.contract.methods[functionName](
+      ...arg,
+    ).call();
+    // Note that this script is intended to be used for bash scripting and
+    // one would usually grep for the results of this execution. Thus,
+    // the raw output is printed rather than include irrelevant log details.
+    // eslint-disable-next-line no-console
+    console.log(info);
     callback();
   } catch (error) {
     callback(error);
