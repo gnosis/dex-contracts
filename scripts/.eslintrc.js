eed23b233eea639c7e5e1378e11f3c7c021ca0ae
Ben Smith <bh2smith@gmail.com>
2020-06-02 20:40:05 +0200

    No longer need to override the no-console for js files (cuz there are none!)

--- scripts/.eslintrc.js
+++ scripts/.eslintrc.js
@@ -2,12 +2,4 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-var-requires": "off",
   },
-  overrides: [
-    {
-      files: ["*.js"],
-      rules: {
-        "no-console": "off",
-      },
-    },
-  ],
 }
