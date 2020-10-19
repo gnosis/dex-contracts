00ac7c87b5197a36b18d2af99738837e0f9edf4a
Ben Smith <bh2smith@gmail.com>
2020-10-19 11:24:52 +0200

    rename migration script

---
+++ src/migration/migrate_BatchExchange.js
@@ -0,0 +1,69 @@
+const { isDevelopmentNetwork, getArtifactFromNpmImport, getArtifactFromBuildFolderOrImport } = require("./utilities.js")
+const deployOwl = require("@gnosis.pm/owl-token/src/migrations-truffle-5/3_deploy_OWL")
+
+async function migrate({
+  BatchExchange,
+  artifacts,
+  deployer,
+  network,
+  account,
+  web3,
+  forceRedeploy,
+  maxTokens = 2 ** 16 - 1,
+  feeTokenAddress = null,
+}) {
+  let feeToken
+
+  const BiMap = getArtifactFromNpmImport(
+    "@gnosis.pm/solidity-data-structures/build/contracts/IdToAddressBiMap",
+    deployer,
+    account
+  )
+  const IterableAppendOnlySet = getArtifactFromNpmImport(
+    "@gnosis.pm/solidity-data-structures/build/contracts/IterableAppendOnlySet",
+    deployer,
+    account
+  )
+
+  if (isDevelopmentNetwork(network)) {
+    await deployOwl({
+      artifacts,
+      deployer,
+      network,
+      account,
+      web3,
+    })
+    await deployer.deploy(BiMap)
+    await deployer.deploy(IterableAppendOnlySet)
+  } else {
+    await BiMap.deployed()
+    await IterableAppendOnlySet.deployed()
+  }
+
+  if (feeTokenAddress == null) {
+    const TokenOWLProxy = getArtifactFromBuildFolderOrImport(
+      artifacts,
+      deployer,
+      account,
+      "@gnosis.pm/owl-token/build/contracts/TokenOWLProxy"
+    )
+    feeToken = await TokenOWLProxy.deployed()
+    feeTokenAddress = feeToken.address
+  }
+
+  // When external projects depend on us we don't want to redeploy e.g. on Rinkeby/Mainnet
+  if (!BatchExchange.isDeployed() || forceRedeploy) {
+    //linking libraries
+    await deployer.link(BiMap, BatchExchange)
+    await deployer.link(IterableAppendOnlySet, BatchExchange)
+
+    // eslint-disable-next-line no-console
+    console.log("Deploy BatchExchange contract")
+    await deployer.deploy(BatchExchange, maxTokens, feeTokenAddress)
+  }
+  // eslint-disable-next-line no-console
+  console.log("  - BatchExchange at address:", BatchExchange.address)
+  return BatchExchange
+}
+
+module.exports = migrate
